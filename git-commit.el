32a725935e9091f1c2ec291b7d7ceff8a77534cd
Florian Ragwitz <rafl@debian.org>
2010-07-06 23:00:30 +0200

    Improve documentation

--- git-commit.el
+++ git-commit.el
@@ -561,13 +561,13 @@ providing commands to do common tasks, and by highlighting the
 basic structure of and errors in git commit messages.
 
 Commands:\\<git-commit-map>
-\\[git-commit-commit]   git-commit-commit  Finish editing and commit
-\\[git-commit-signoff]   git-commit-signoff   Insert a Signed-off-by header
-\\[git-commit-ack]   git-commit-ack   Insert an Acked-by header
-\\[git-commit-test]   git-commit-test   Insert a Tested-by header
-\\[git-commit-review]   git-commit-review   Insert a Reviewed-by header
-\\[git-commit-cc]   git-commit-cc   Insert a Cc header
-\\[git-commit-reported]   git-commit-reported   Insert a Reported-by header
+\\[git-commit-commit]   `git-commit-commit'  Finish editing and commit
+\\[git-commit-signoff]   `git-commit-signoff'   Insert a Signed-off-by header
+\\[git-commit-ack]   `git-commit-ack'   Insert an Acked-by header
+\\[git-commit-test]   `git-commit-test'   Insert a Tested-by header
+\\[git-commit-review]   `git-commit-review'   Insert a Reviewed-by header
+\\[git-commit-cc]   `git-commit-cc'   Insert a Cc header
+\\[git-commit-reported]   `git-commit-reported'   Insert a Reported-by header
 
 Turning on git commit calls the hooks in `git-commit-mode-hook'."
   (interactive)
