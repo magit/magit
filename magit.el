fc01cd5936b39874c14bd1dd01cc8848ccc5126d
Timo Juhani Lindfors <timo.lindfors@iki.fi>
2010-09-02 20:52:25 +0300

    magit: [PATCH] Improve documentation of minor modes. Keymaps are now included.
    Hi,
    
    this patch improves documentation of minor modes. Notably it includes
    the keymaps so that C-h m shows them depending on the mode.
    
    From 17ac227df3428af49f4400ef27b5a05ca030bdee Mon Sep 17 00:00:00 2001
    From: Timo Juhani Lindfors <timo.lindfors@iki.fi>
    Date: Thu, 2 Sep 2010 20:46:27 +0300
    Subject: [PATCH] Improve documentation of minor modes. Keymaps are now included.

--- magit.el
+++ magit.el
@@ -2479,7 +2479,9 @@ insert a line to tell how to insert more of them"
 		       "-p" commit)))
 
 (define-minor-mode magit-commit-mode
-    "Minor mode to view git commit"
+    "Minor mode to view a git commit.
+
+\\{magit-commit-mode-map}"
   :group magit
   :init-value ()
   :lighter ()
@@ -2654,7 +2656,9 @@ insert a line to tell how to insert more of them"
 	(magit-run* (list "git" "init"))))))
 
 (define-minor-mode magit-status-mode
-    "Minor mode for looking at git status"
+    "Minor mode for looking at git status.
+
+\\{magit-status-mode-map}"
   :group magit
   :init-value ()
   :lighter ()
@@ -3479,7 +3483,9 @@ With prefix argument, changes in staging area are kept.
 (defvar magit-currently-shown-stash nil)
 
 (define-minor-mode magit-stash-mode
-    "Minor mode for looking at git status"
+    "Minor mode for looking at a git stash.
+
+\\{magit-stash-mode-map}"
   :group magit
   :init-value ()
   :lighter ()
@@ -3645,7 +3651,9 @@ With a non numeric prefix ARG, show all entries"
 	     "--"))))
 
 (define-minor-mode magit-log-mode
-    "Minor mode for looking at git status"
+    "Minor mode for looking at git log.
+
+\\{magit-log-mode-map}"
   :group magit
   :init-value ()
   :lighter ()
@@ -3726,7 +3734,9 @@ This is only non-nil in reflog buffers.")
 		       args)))
 
 (define-minor-mode magit-reflog-mode
-    "Minor mode for looking at git status"
+    "Minor mode for looking at git reflog.
+
+\\{magit-reflog-mode-map}"
   :group magit
   :init-value ()
   :lighter ()
@@ -3757,7 +3767,9 @@ This is only non-nil in reflog buffers.")
 		       "diff" (magit-diff-U-arg) args)))
 
 (define-minor-mode magit-diff-mode
-    "Minor mode for looking at git status"
+    "Minor mode for looking at a git diff.
+
+\\{magit-diff-mode-map}"
   :group magit
   :init-value ()
   :lighter ()
@@ -3858,7 +3870,9 @@ This is only meaningful in wazzup buffers.")
 		  (magit-set-section-info ref section))))))))))
 
 (define-minor-mode magit-wazzup-mode
-    "Minor mode for looking at git status"
+    "Minor mode for looking at commits that could be merged from other branches.
+
+\\{magit-wazzup-mode-map}"
   :group magit
   :init-value ()
   :lighter ()
