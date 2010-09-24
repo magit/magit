magit/magit

commit 81bb2ba2d32fb3bbbc2baaba60b492afe372d831

magit-rebase-info: remove dead code and add >= 1.6 support
Magit's "R" feature didn't work with a Git newer than 1.6. It was only
checking for files which were removed from git.git in v1.6.0-rc0~56
[1].

1. http://github.com/git/git/commit/v1.6.0-rc0~56

Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

magit.el
@@ -2852,21 +2852,14 @@ With a prefix-arg, the merge will be squashed.
 ;;; Rebasing
 
 (defun magit-rebase-info ()
-  (cond ((file-exists-p ".git/rebase-apply")
-	 (list (magit-name-rev
-		(car (magit-file-lines ".git/rebase-apply/onto")))
-	       (car (magit-file-lines ".git/rebase-apply/next"))
-	       (car (magit-file-lines ".git/rebase-apply/last"))))
-	((file-exists-p ".dotest")
-	 (list (magit-name-rev (car (magit-file-lines ".dotest/onto")))
-	       (car (magit-file-lines ".dotest/next"))
-	       (car (magit-file-lines ".dotest/last"))))
-	((file-exists-p ".git/.dotest-merge")
-	 (list (car (magit-file-lines ".git/.dotest-merge/onto_name"))
-	       (car (magit-file-lines ".git/.dotest-merge/msgnum"))
-	       (car (magit-file-lines ".git/.dotest-merge/end"))))
-	(t
-	 nil)))
+  "Returns a list indicating the state of an in-progress rebase,
+if any."
+  (cond ((file-exists-p ".git/rebase-merge")
+         (list
+          (magit-name-rev (car (magit-file-lines ".git/rebase-merge/message")))
+          (length (magit-file-lines ".git/rebase-merge/done"))
+          (length (magit-file-lines ".git/rebase-merge/git-rebase-todo.backup"))))
+	(t nil)))
 
 (defun magit-rebase-step ()
   (interactive)
@@ -2877,7 +2870,7 @@ With a prefix-arg, the merge will be squashed.
 	      (magit-run-git "rebase" (magit-rev-to-git rev))))
       (let ((cursor-in-echo-area t)
             (message-log-max nil))
-        (message "Rebase in progress.  Abort, Skip, or Continue? ")
+        (message "Rebase in progress. Abort, Skip, or Continue? ")
         (let ((reply (read-event)))
           (case reply
             ((?A ?a)

commit 823c85b32bb1c1a56e564575b1353d8924df0337

magit-rebase-info: solve the 'The "R" rebase method needs a small fix' issue
Fix the bug in magit-rebase-info that I introduced in 81bb2ba. Here's
the description of the bug from a message of mine to the mailing list:

    I sent an incomplete fix for the "R" rebase method recently:

       http://github.com/philjackson/magit/pull/59

    But Phil pulled it in as-is:

       http://github.com/philjackson/magit/commit/81bb2ba2d32fb3bbbc2baaba60b492afe372d831

    Anyway, it needs a small fixup, this:

       (length (magit-file-lines ".git/rebase-merge/git-rebase-todo.backup"))

    Will be way off, because it'll count the (length) of lines like:

       ("pick cb925f0 check message..." "pick 2f811b6 take done length"
    "pick 29332e6 use backup" "" "# Rebase 19e2dd7..29332e6 onto 19e2dd7"
    "#" ...

    What it should do instead so we can get the correct count is to only
    count up until the "" (uninclusive) so the above example would return
    3.

    I couldn't find an idiomatic way of doing this in elisp, I could with
    the (loop) macro, but that'd require cl.el.

As it turns out magit already needs cl.el, so I can just use (loop) to
fix this.

Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

magit.el
@@ -2888,9 +2888,18 @@ With a prefix-arg, the merge will be squashed.
 if any."
   (cond ((file-exists-p ".git/rebase-merge")
          (list
+          ;; The commit message of the commit we're at
           (magit-name-rev (car (magit-file-lines ".git/rebase-merge/message")))
+
+          ;; How many commits we've gone through
           (length (magit-file-lines ".git/rebase-merge/done"))
-          (length (magit-file-lines ".git/rebase-merge/git-rebase-todo.backup"))))
+
+          ;; How many commits we have in total, without the comments
+          ;; at the end of git-rebase-todo.backup
+          (let ((todo-lines-with-comments (magit-file-lines ".git/rebase-merge/git-rebase-todo.backup")))
+            (loop for i in todo-lines-with-comments
+                  until (string= "" i)
+                  count i))))
 	(t nil)))
 
 (defun magit-rebase-step ()