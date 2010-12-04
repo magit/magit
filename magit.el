8cb885ba4e691a806284f5c5fb2db2d089593569
Rémi Vanicat <vanicat@debian.org>
2010-12-04 06:08:02 +0100

    Make magit-get-branch return a branch name instead of an sha1.

--- magit.el
+++ magit.el
@@ -2981,7 +2981,7 @@ If the branch is the current one, offers to switch to `master' first.
   (magit-section-case (item info)
     ((wazzup commit)
      (magit-section-info (magit-section-parent item)))
-    ((commit) (substring info 0 8))
+    ((commit) (magit-name-rev (substring info 0 8)))
     ((wazzup) info)))
 
 (defun magit-merge (revision)
