magit/magit

commit 18d33922ee48d2f8935985725b50a9302d57cefc

Self documentation for magit-fetch and magit-fetch-remote.

magit.el
@@ -3336,14 +3336,15 @@ Uncomitted changes in both working tree and staging area are lost.
 
 ;;; Updating, pull, and push
 
-(magit-define-command fetch (&optional remote)
-  "Run fetch."
-  (interactive)
-  (magit-run-git-async "fetch" (or remote
-                                   (magit-read-remote))))
+(magit-define-command fetch (remote)
+  "Fetch from REMOTE."
+  (interactive (magit-read-remote))
+  (magit-run-git-async "fetch" (or remote)))
 
 (magit-define-command fetch-current ()
-  "Run fetch."
+  "Run fetch for default remote.
+
+If there is no default remote, ask for one."
   (interactive)
   (magit-fetch (or (magit-get-current-remote)
                    (magit-read-remote))))

commit 666dd4b0ba7ab0b12cdd2a10e3f78f10da8e9cea

Fix bug introduced in 18d33922ee48d2f8935985725b50a9302d57cefc
Interactive spec must evaluate to a *list* of arguments to pass to the function.

magit.el
@@ -3342,8 +3342,8 @@ Uncomitted changes in both working tree and staging area are lost.
 
 (magit-define-command fetch (remote)
   "Fetch from REMOTE."
-  (interactive (magit-read-remote))
-  (magit-run-git-async "fetch" (or remote)))
+  (interactive (list (magit-read-remote)))
+  (magit-run-git-async "fetch" remote))
 
 (magit-define-command fetch-current ()
   "Run fetch for default remote.