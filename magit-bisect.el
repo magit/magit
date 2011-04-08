magit/magit

commit 5d1336a9a0bcf800359c051f88879fdfb58d383a

Key mode functions do not pass prefix args through

magit-bisect.el
@@ -3,7 +3,9 @@
 (put 'magit--bisect-info 'permanent-local t)
 
 (defun magit--bisecting-p (&optional required-status)
-  "Return t if a bisect session is running"
+  "Return t if a bisect session is running.
+If REQUIRED-STATUS is not nil then the current status must also
+match REQUIRED-STATUS."
   (and (file-exists-p (concat (magit-get-top-dir default-directory)
                           ".git/BISECT_LOG"))
        (or (not required-status)
@@ -83,18 +85,12 @@
     (error "Not bisecting"))
   (magit--bisect-cmd "bad"))
 
-(defun magit-bisect-skip (&optional pfx)
-  "Tell git to skip the current revision during a bisect session.
-With prefix let the user enter the revisions to skip."
+(defun magit-bisect-skip ()
+  "Tell git to skip the current revision during a bisect session."
   (interactive "P")
   (unless (magit--bisecting-p 'running)
     (error "Not bisecting"))
-  (let ((args '("skip")))
-    (if pfx
-        (setq args (append args
-                           (split (magit-completing-read "Revision(s) to skip: " nil)
-                                  " "))))
-    (apply 'magit--bisect-cmd args)))
+  (magit--bisect-cmd "skip"))
 
 (defun magit-bisect-log ()
   "Show the bisect log"

magit.texi
@@ -723,8 +723,7 @@ and magit will update its status buffer accordingly.
 
 You can tell git that the current revision is good with @kbd{B g},
 that it is bad with @kbd{B b} or that git should skip it with @kbd{B
-k}. You can specify revisions or ranges to skip by prefixing the
-latter.
+k}.
 
 The current status can be shown as a log with @kbd{B l}. It contains
 the revisions that have already been tested and your decisions about

commit 3b6c2e94ba6a224a3e47048b05774b0af7fa4484

magit-bisect-skip doesn't take an argument; so don't pass one when calling it interactively
Fix for bug introduced in 5d1336a

@pjweisberg thanks

magit-bisect.el
@@ -87,7 +87,7 @@ match REQUIRED-STATUS."
 
 (defun magit-bisect-skip ()
   "Tell git to skip the current revision during a bisect session."
-  (interactive "P")
+  (interactive)
   (unless (magit--bisecting-p 'running)
     (error "Not bisecting"))
   (magit--bisect-cmd "skip"))