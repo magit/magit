magit/magit

commit 14fdc8dc7ccb05c48552299224c49ade2b7d28b2

make rewrite start on top of base, not below
Commit cd4f15f314cfaa74 had the perfectly valid intention to prevent
magit-rewrite-start from writing parentless commits.

Yet as an unfortunate side-effect, rewrite and rebase ("r" and "R"
respectively) now behave inconsistently: while rebasing starts at
REV (as would be expected), rewriting starts at REV~1 (rather
counter-intuitive).

This does not bode well for cherry-picking onto a public repo either.

The manpage states correctly that the base commit is included in the
rewrite operation:

>> Typing `r s' will start a rewrite operation.  You will be prompted
>> for a _base_ commit, and all commits between the current head and
>> this commit are put in a list of _Pending commits_ (including the
>> base commit).  The current head will then be reset to the parent of
>> the base commit.

But since the starting point is explicitly called a "_base_ commit" (I
imagine most people think about a base as something to build *upon*,
not *below*), something's due for revision.

magit.el
@@ -3353,8 +3353,9 @@ Uncomitted changes in both working tree and staging area are lost.
   (or (not (magit-read-rewrite-info))
       (error "Rewrite in progress"))
   (let* ((orig (magit-rev-parse "HEAD"))
-	 (base (or (car (magit-commit-parents from))
-		   (error "Can't rewrite a commit without a parent, sorry")))
+         (base (if (car (magit-commit-parents from))
+                   from
+                 (error "Can't rewrite a commit without a parent, sorry")))
 	 (pending (magit-git-lines "rev-list" (concat base ".."))))
     (magit-write-rewrite-info `((orig ,orig)
 				(pending ,@(mapcar #'list pending))))

magit.texi
@@ -622,9 +622,8 @@ prefix key.
 
 Typing @kbd{r b} will start a rewrite operation.  You will be prompted
 for a @emph{base} commit, and all commits between the current head and
-this commit are put in a list of @emph{Pending commits} (including the
-base commit).  The current head will then be reset to the parent of
-the base commit.
+this commit are put in a list of @emph{Pending commits}.  The current
+head will then be reset to the base commit.
 
 You would then typically use @kbd{a} and @kbd{A} to cherry pick
 commits from the list of pending commits in the desired order, until

commit 2d29228a3ce6d21b5f4a583713461e6f157065d0

fix rewriting bug introduced in 14fdc8dc7ccb05c4
Since the rewrite base isn't rewritten itself anymore, verifying if it
has any parents is unnecessary and counter-productive:

- Unnecessary because there's no chance of erroneously writing
  parentless commits. It is itself the parent for subsequent commits.

- Counter-productive because it results in faulty behaviour. It
  renders us unable to use the very first commit as rewrite base.

magit.el
@@ -3432,9 +3432,7 @@ Uncomitted changes in both working tree and staging area are lost.
   (or (not (magit-read-rewrite-info))
       (error "Rewrite in progress"))
   (let* ((orig (magit-rev-parse "HEAD"))
-         (base (if (car (magit-commit-parents from))
-                   from
-                 (error "Can't rewrite a commit without a parent, sorry")))
+         (base from)
 	 (pending (magit-git-lines "rev-list" (concat base ".."))))
     (magit-write-rewrite-info `((orig ,orig)
 				(pending ,@(mapcar #'list pending))))