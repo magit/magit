magit/magit

commit fe3d63f9e075a7b4de64f385d78d7695f0625c92

Fix `magit-save-buffers-predicate-tree-only' when checking unsaved buffers across multiple repositories.
Old code would prompt to save *any* dirty buffer which was part of a git
repository, rather than just the specific repo for which `magit-status'
was invoked. Fixed to check only against magit-topdir of the invocation
directory.

magit.el
@@ -3329,7 +3329,8 @@ to consider it or not when called with that buffer current."
   "Only prompt to save buffers which are within the current git project (as
   determined by the dir passed to `magit-status'."
   (and buffer-file-name
-       (magit-get-top-dir (file-name-directory buffer-file-name))))
+       (eq (magit-get-top-dir dir)
+           (magit-get-top-dir (file-name-directory buffer-file-name)))))
 
 ;;;###autoload
 (defun magit-status (dir)

commit 53a54a5400817b4de5c3099ab7aeee15a720efe6

fix bug introduced by commit fe3d63f
since default-directory is set in `magit-mode-init', we can use this value as
a reference for each action performed from a buffer with magit-mode, which
should cover all the cases.

magit.el
@@ -3307,6 +3307,8 @@ PREPEND-REMOTE-NAME is non-nil."
 \\{magit-status-mode-map}"
   :group 'magit)
 
+(defvar magit-default-directory nil)
+
 (defun magit-save-some-buffers (&optional msg pred)
   "Save some buffers if variable `magit-save-some-buffers' is non-nil.
 If variable `magit-save-some-buffers' is set to 'dontask then
@@ -3322,8 +3324,8 @@ If PRED is t, then certain non-file buffers will also be considered.
 If PRED is a zero-argument function, it indicates for each buffer whether
 to consider it or not when called with that buffer current."
   (interactive)
-  (let ((predicate-function (or pred magit-save-some-buffers-predicate)))
-
+  (let ((predicate-function (or pred magit-save-some-buffers-predicate))
+        (magit-default-directory default-directory))
     (if magit-save-some-buffers
         (save-some-buffers
          (eq magit-save-some-buffers 'dontask)
@@ -3340,8 +3342,8 @@ to consider it or not when called with that buffer current."
   "Only prompt to save buffers which are within the current git project (as
   determined by the dir passed to `magit-status'."
   (and buffer-file-name
-       (eq (magit-get-top-dir dir)
-           (magit-get-top-dir (file-name-directory buffer-file-name)))))
+       (string= (magit-get-top-dir magit-default-directory)
+                (magit-get-top-dir (file-name-directory buffer-file-name)))))
 
 ;;;###autoload
 (defun magit-status (dir)