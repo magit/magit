8d8030f88172f90435cf04e4253f75defd3d588e
Nicolas Dudebout <nicolas.dudebout@gatech.edu>
2012-01-11 11:56:10 -0500

    Fixed the problem mentioned by @dabrahams in commit b2daab9.

--- magit.el
+++ magit.el
@@ -4267,7 +4267,7 @@ This means that the eventual commit does 'git commit --allow-empty'."
     (magit-log-edit-mode)
     (message "Type C-c C-c to %s (C-c C-k to cancel)." operation)))
 
-(defun magit-log-edit (amend-p)
+(defun magit-log-edit (&optional amend-p)
   "Brings up a buffer to allow editing of commit messages. Given
 a prefix arg will set the amend flag for the commit buffer.
 
@@ -4280,29 +4280,32 @@ continue it.
   ;; suggest to continue the rebase. Git will rebuke you and exit with
   ;; error code, so suggest it only if theres absolutely nothing else
   ;; to do and rebase is ongoing.
-  (if (magit-everything-clean-p)
-      (when (and (magit-rebase-info)
-                 (y-or-n-p "Rebase in progress.  Continue it? "))
-        (magit-run-git-async "rebase" "--continue"))
-    ;; If there's nothing staged, set commit flag to `nil', thus
-    ;; avoiding unnescessary popping up of the log edit buffer in case
-    ;; when user chose to forgo commiting all unstaged changes
-    (let ((perform-commit-p (magit-anything-staged-p)))
-      (when (and magit-commit-all-when-nothing-staged
-                 (not perform-commit-p))
-        (cond ((eq magit-commit-all-when-nothing-staged 'ask-stage)
-               (when (y-or-n-p "Nothing staged.  Stage everything now? ")
-                 (setq perform-commit-p t)
-                 (magit-stage-all)))
-              ((not (magit-log-edit-get-field 'commit-all))
-               (when (or (eq magit-commit-all-when-nothing-staged t)
-                         (y-or-n-p
-                          "Nothing staged.  Commit all unstaged changes? "))
-                 (magit-log-edit-set-field 'commit-all "yes")
-                 (setq perform-commit-p t)))))
-      (when perform-commit-p
-        (when amend-p (magit-log-edit-toggle-amending))
-        (magit-pop-to-log-edit "commit")))))
+  (if (and (magit-everything-clean-p)
+             (magit-rebase-info)
+             (y-or-n-p "Rebase in progress.  Continue it? "))
+    (magit-run-git-async "rebase" "--continue")
+
+  ;; If there's nothing staged, set commit flag to `nil', thus
+  ;; avoiding unnescessary popping up of the log edit buffer in case
+  ;; when user chose to forgo commiting all unstaged changes
+  (let ((perform-commit-p (or (magit-anything-staged-p)
+                              amend-p)))
+    (when (and magit-commit-all-when-nothing-staged
+               (not (magit-everything-clean-p))
+               (not (magit-anything-staged-p)))
+      (cond ((eq magit-commit-all-when-nothing-staged 'ask-stage)
+             (when (y-or-n-p "Nothing staged.  Stage everything now? ")
+               (setq perform-commit-p t)
+               (magit-stage-all)))
+            ((not (magit-log-edit-get-field 'commit-all))
+             (when (or (eq magit-commit-all-when-nothing-staged t)
+                       (y-or-n-p
+                        "Nothing staged.  Commit all unstaged changes? "))
+               (magit-log-edit-set-field 'commit-all "yes")
+               (setq perform-commit-p t)))))
+    (when perform-commit-p
+      (when amend-p (magit-log-edit-toggle-amending))
+      (magit-pop-to-log-edit "commit")))))
 
 (defun magit-add-log ()
   (interactive)
