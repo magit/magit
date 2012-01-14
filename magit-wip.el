f65f2ea9900f61fc708d4ca8d42c4212485a5484
Ryan C. Thompson <rct@thompsonclan.org>
2012-01-14 11:46:50 -0800

    Add comments and commentary for magit-wip.el

--- magit-wip.el
+++ magit-wip.el
@@ -1,3 +1,47 @@
+;;; magit-wip.el --- git-wip plug-in for Magit
+
+;; Copyright (C) 2012  Ryan C. Thompson
+;;
+;; Magit is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; Magit is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with Magit.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This plug-in provides special highlighting of git-wip refs in Magit
+;; log buffers.
+
+;; Available commands:
+;; - `magit-wip-mode': Enable or disable the mode
+
+;; TODO:
+;; - Provide an interface for `git wip log'
+
+;;; Code:
+
+(require 'magit)
+
+;;; Customizables:
+
+(define-minor-mode magit-wip-mode
+  "Magit support fot git-wip.
+
+Currently, this will just give git-wip refs a special appearance
+in Magit log buffers."
+  :group 'magit
+  :global t
+  :require 'magit-wip
+  (magit-wip-setup-namespaces))
+
 (defface magit-log-head-label-wip
   '((((class color) (background light))
      :box t
@@ -9,6 +53,8 @@
      :foreground "LightSkyBlue4"))
   "Face for git-wip labels shown in log buffer."
   :group 'magit-faces)
+
+;;; Common code:
 
 (defun magit-log-get-wip-color (suffix)
   (list (concat "(WIP) " suffix)
@@ -23,15 +69,5 @@
         (remover (lambda (ns) (setq magit-refs-namespaces (delete ns magit-refs-namespaces)))))
     (mapc (if magit-wip-mode adder remover)
           magit-wip-refs-namespaces)))
-
-(define-minor-mode magit-wip-mode
-  "Magit support fot git-wip.
-
-Currently, this will just give git-wip refs a special appearance
-in Magit log buffers."
-  :group 'magit
-  :global t
-  :require 'magit-wip
-  (magit-wip-setup-namespaces))
 
 (provide 'magit-wip)
