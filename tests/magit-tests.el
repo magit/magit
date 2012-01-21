264ef980af57909158d0c691bef018459a6d0529
Yann Hodique <yann.hodique@gmail.com>
2012-01-21 23:01:51 +0100

    add basic tests for unstaged and staged files

--- tests/magit-tests.el
+++ tests/magit-tests.el
@@ -18,6 +18,14 @@
            ,@body)
        (delete-directory ,repo t))))
 
+(defun magit-tests-section-has-item-title (title &optional section-path)
+  (let ((children (magit-section-children
+                   (or (and section-path
+                            (magit-find-section section-path
+                                                magit-top-section))))))
+    (should (member title
+                    (mapcar 'magit-section-title children)))))
+
 (ert-deftest magit-init-test ()
   (with-temp-git-repo repo
     (should (magit-git-repo-p repo))))
@@ -35,3 +43,20 @@
         (magit-init nested-repo)
         (should (magit-git-repo-p nested-repo))))
     (should (magit-git-repo-p repo))))
+
+(ert-deftest magit-untracked-file ()
+  (let ((dummy-filename "foo"))
+    (with-temp-git-repo repo
+      (with-temp-buffer
+        (write-file (format "%s/%s" repo dummy-filename)))
+      (magit-status repo)
+      (magit-tests-section-has-item-title dummy-filename '(untracked)))))
+
+(ert-deftest magit-staged-file-from-all ()
+  (let ((dummy-filename "foo"))
+    (with-temp-git-repo repo
+      (with-temp-buffer
+        (write-file (format "%s/%s" repo dummy-filename)))
+      (magit-status repo)
+      (magit-stage-all t)
+      (magit-tests-section-has-item-title dummy-filename '(staged)))))
