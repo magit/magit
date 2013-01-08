da6083e93e285e9ef7aa94d331b4d8126d6b88bb
Sebastian Wiesner <lunaryorn@gmail.com>
2013-01-08 12:32:07 +0100

    Release git-commit-mode 0.10

--- git-commit-mode.el
+++ git-commit-mode.el
@@ -7,7 +7,7 @@
 ;;      Florian Ragwitz <rafl@debian.org>
 ;; Maintainer: Sebastian Wiesner <lunaryorn@gmail.com>
 ;; URL: https://github.com/lunaryorn/git-modes
-;; Version: 0.9
+;; Version: 0.10
 ;; Keywords: convenience vc git
 
 ;; This file is not part of GNU Emacs.
