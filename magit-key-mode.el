55aaac2fe5c58d8024609835b6cc15b925ca8c0f
Takafumi Arakaki <aka.tkf@gmail.com>
2013-05-05 16:22:02 +0200

    Add --branches/--remotes support to logging

--- magit-key-mode.el
+++ magit-key-mode.el
@@ -76,7 +76,9 @@ command-line).")
       ("=<" "Before" "--before=" read-from-minibuffer)
       ("=s" "Pickaxe search" "-S" read-from-minibuffer)
       ("=a" "Author" "--author=" read-from-minibuffer)
-      ("=g" "Grep" "--grep=" read-from-minibuffer)))
+      ("=g" "Grep" "--grep=" read-from-minibuffer)
+      ("=b" "Branches" "--branches=" read-from-minibuffer)
+      ("=R" "Remotes" "--remotes=" read-from-minibuffer)))
 
     (running
      (actions
