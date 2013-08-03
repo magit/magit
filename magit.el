bcce594d86930ca5d1af43bbe91ec532cd19db43
Jonas Bernoulli <jonas@bernoul.li>
2013-08-03 17:19:13 +0200

    magit-read-file-from-rev: fix regression

--- magit.el
+++ magit.el
@@ -1500,7 +1500,7 @@ according to `magit-remote-ref-format'"
 
 (defun magit-read-file-from-rev (revision)
   (magit-completing-read (format "Retrieve file from %s: " revision)
-                         (magit-git-lines "ls-tree" "--name-only" "HEAD")
+                         (magit-git-lines "ls-tree" "--name-only" revision)
                          nil 'require-match
                          nil 'magit-read-file-hist
                          (when buffer-file-name
