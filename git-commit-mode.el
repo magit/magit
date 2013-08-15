76af77a2cd31f17f17773249bf05fd370f924c54
Jonas Bernoulli <jonas@bernoul.li>
2013-08-16 00:52:40 +0200

    use elisp style headings in Commentary

--- git-commit-mode.el
+++ git-commit-mode.el
@@ -30,8 +30,8 @@
 
 ;; A major mode for editing Git commit messages.
 
-;; * Formatting
-;;
+;;;; Formatting
+
 ;; Highlight the formatting of git commit messages and indicate errors according
 ;; to the guidelines for commit messages (see
 ;; http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html).
@@ -42,8 +42,8 @@
 ;; Enable `auto-fill-mode' and set the `fill-column' to 72 according to the
 ;; aforementioned guidelines.
 
-;; * Headers
-;;
+;;;; Headers
+
 ;; Provide commands to insert standard headers into commit messages.
 ;;
 ;; - C-c C-s inserts Signed-off-by (`git-commit-signoff').
@@ -53,8 +53,8 @@
 ;; - C-c C-o inserts Cc (`git-commit-cc').
 ;; - C-c C-p inserts Reported-by (`git-commit-reported').
 
-;; * Committing
-;;
+;;;; Committing
+
 ;; C-c C-c finishes a commit.  By default this means to save and kill the
 ;; buffer.  Customize `git-commit-commit-function' to change this behaviour.
 ;;
