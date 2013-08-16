b92dd658fe3c0e1043a69a75707dd77bb3f8ff0d
Jonas Bernoulli <jonas@bernoul.li>
2013-08-16 18:13:10 +0200

    set Version keyword in library headers

--- git-commit-mode.el
+++ git-commit-mode.el
@@ -6,6 +6,7 @@
 ;; Authors: Sebastian Wiesner <lunaryorn@gmail.com>
 ;;	Florian Ragwitz <rafl@debian.org>
 ;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
+;; Version: 0.14.0
 ;; Homepage: https://github.com/magit/git-modes
 ;; Keywords: convenience vc git
 
--- git-rebase-mode.el
+++ git-rebase-mode.el
@@ -5,6 +5,7 @@
 
 ;; Author: Phil Jackson <phil@shellarchive.co.uk>
 ;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
+;; Version: 0.14.0
 ;; Homepage: https://github.com/magit/git-modes
 ;; Keywords: convenience vc git
 
