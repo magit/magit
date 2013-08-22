c36bf59a39177fbff881b59a08284b6e1442472e
Jonas Bernoulli <jonas@bernoul.li>
2013-08-22 19:54:52 +0200

    rename magit-commit{,-confirm}-{cancel => abort}
    Rename `magit-commit-cancel' to `magit-commit-abort', and rename
    `magit-commit-confirm-cancel' to `magit-commit-confirm-abort' for
    consistency with `magit-rebase-abort'.

--- git-commit-mode.el
+++ git-commit-mode.el
@@ -152,7 +152,7 @@ confirmation before committing."
   :type '(choice (const :tag "On style errors" t)
                  (const :tag "Never" nil)))
 
-(defcustom git-commit-confirm-cancel t
+(defcustom git-commit-confirm-abort t
   "Whether to ask for confirmation before cancelling a commit.
 
 If t, ask for confirmation before cancelling a commit, unless
@@ -181,15 +181,15 @@ Return t, if the commit was successful, or nil otherwise."
       (funcall git-commit-commit-function)
     (message "Commit canceled due to stylistic errors.")))
 
-(defun git-commit-cancel (&optional force)
+(defun git-commit-abort (&optional force)
   "Abort the commit.
 
-Ask for confirmation depending on `git-commit-confirm-cancel'.
+Ask for confirmation depending on `git-commit-confirm-abort'.
 If FORCE is non-nil or if a raw prefix arg is given, cancel
 without confirming."
   (interactive "P")
   (when (or force
-            (not git-commit-confirm-cancel)
+            (not git-commit-confirm-abort)
             (yes-or-no-p
              "Cancel commit (your commit message will be lost)?"))
     (erase-buffer)
@@ -466,7 +466,7 @@ Known comment headings are provided by `git-commit-comment-headings'."
 (defvar git-commit-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "C-c C-c") 'git-commit-commit)
-    (define-key map (kbd "C-c C-k") 'git-commit-cancel)
+    (define-key map (kbd "C-c C-k") 'git-commit-abort)
     (define-key map (kbd "C-c C-s") 'git-commit-signoff)
     (define-key map (kbd "C-c C-a") 'git-commit-ack)
     (define-key map (kbd "C-c C-t") 'git-commit-test)
