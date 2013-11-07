c3508b18d80267f5787bbf010524ee67c9a04dcf
Jonas Bernoulli <jonas@bernoul.li>
2013-11-07 01:41:00 +0100

    magit-mark-commit: run new magit-mark-commit-hook
    In `magit-mark-commit run the new hook `magit-mark-commit-hook'.
    Do not explicitly declare the hook variable, at this point it is
    not intended to be used by third-party code.

--- magit.el
+++ magit.el
@@ -6491,7 +6491,8 @@ With a prefix argument, visit in other window."
       ((commit)
        (setq magit-marked-commit
              (if (equal magit-marked-commit info) nil info)))))
-  (magit-refresh-marked-commits))
+  (magit-refresh-marked-commits)
+  (run-hooks 'magit-mark-commit-hook))
 
 ;;;; Kill
 
