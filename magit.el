d9639d41a5b43c97ac5e5da6ea0b0b3c32e38fcf
Jonas Bernoulli <jonas@bernoul.li>
2013-11-14 13:15:22 +0100

    magit-mark-item: don't refresh buffer
    Commit 00718df7ba4cfdcc04cb6e87f5c0c7c3fdc41bd7 was intended
    to do that but just contained a TODO instead of an actual change.

--- magit.el
+++ magit.el
@@ -6854,7 +6854,7 @@ With a prefix argument, visit in other window."
   (interactive "P")
   (if unmark
       (setq magit-marked-commit nil)
-    (magit-section-action (item info "mark") ; ?
+    (magit-section-action (item info "mark" t)
       ((commit)
        (setq magit-marked-commit
              (if (equal magit-marked-commit info) nil info)))))
