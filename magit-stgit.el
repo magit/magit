d1afbbcf65b269ce9b65c97ed7540e89768275a3
Lluís Vilanova <vilanova@ac.upc.edu>
2014-01-09 18:56:15 +0100

    stgit: Fix 'magit-run-stgit'
    Use 'magit-call-process' instead of 'magit-call-git'.

--- magit-stgit.el
+++ magit-stgit.el
@@ -90,7 +90,7 @@
 ;;; Utilities
 
 (defun magit-run-stgit (&rest args)
-  (apply #'magit-call-git (cons magit-stgit-executable args))
+  (apply #'magit-call-process magit-stgit-executable args)
   (magit-refresh))
 
 (defun magit-stgit-lines (&rest args)
