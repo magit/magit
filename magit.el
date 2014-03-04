c9d5d66c7f5ed87c232532028fdfa502dfc7b0af
Jonas Bernoulli <jonas@bernoul.li>
2014-03-04 21:14:00 +0100

    magit.el: absorb magit-wip.el

--- magit.el
+++ magit.el
@@ -196,6 +196,10 @@ aborts and returns that value."
   "Extensions to Magit."
   :group 'magit)
 
+(defgroup magit-wip nil
+  "Git-Wip support for Magit."
+  :group 'magit-extensions)
+
 (defgroup magit-faces nil
   "Faces used by Magit."
   :group 'magit
@@ -977,6 +981,28 @@ t          ask if --set-upstream should be used.
   :package-version '(magit . "2.0.0")
   :group 'magit-modes
   :type 'hook)
+
+(defcustom magit-wip-commit-message "WIP %r"
+  "Commit message for git-wip commits.
+
+The following `format'-like specs are supported:
+%f the full name of the file being saved
+%g the root of the git repository
+%r the name of the file being saved,
+   relative to the repository root."
+  :group 'magit-wip
+  :type 'string)
+
+(defcustom magit-wip-echo-area-message "Wrote %f (wip)"
+  "Message shown in the echo area after creating a git-wip commit.
+
+The following `format'-like specs are supported:
+%f the full name of the file being saved
+%g the root of the git repository
+%r the name of the file being saved,
+   relative to the repository root."
+  :group 'magit-wip
+  :type '(choice (const :tag "No message" nil) string))
 
 ;;;; Custom Faces
 
@@ -7584,6 +7610,65 @@ non-nil, then autocompletion will offer directory names."
            (setq result (append result sub)))))
      dict)
     result))
+
+;;;; Wip Minor Mode
+
+(defvar magit-wip-save-mode-lighter " Wip")
+
+;;;###autoload
+(define-minor-mode magit-wip-save-mode
+  "Magit support for committing to a work-in-progress ref.
+
+When this minor mode is turned on and a file is saved inside a
+writable git repository then it is also committed to a special
+work-in-progress ref."
+  :lighter magit-wip-save-mode-lighter
+  (if magit-wip-save-mode
+      (add-hook  'after-save-hook 'magit-wip-save t t)
+    (remove-hook 'after-save-hook 'magit-wip-save t)))
+
+;;;###autoload
+(define-globalized-minor-mode global-magit-wip-save-mode
+  magit-wip-save-mode turn-on-magit-wip-save
+  :group 'magit-wip)
+
+(defun turn-on-magit-wip-save ()
+  "Conditionally turn on magit-wip-save-mode.
+
+Turn on magit-wip-save-mode if the buffer is a file in a git
+repository where wip-save is enabled in git config.
+
+You can activate it with git config magit.extension wip-save."
+  (when (and (buffer-file-name)
+             (magit-get-top-dir)
+             (member "wip-save" (magit-get-all "magit.extension")))
+    (if (magit-git-success "wip" "-h")
+        (magit-wip-save-mode 1)
+      (message "Git command 'git wip' cannot be found"))))
+
+(defun magit-wip-save ()
+  (let* ((filename (expand-file-name (file-truename (buffer-file-name))))
+         (filedir  (file-name-directory filename))
+         (toplevel (magit-get-top-dir filedir))
+         (blobname (file-relative-name filename toplevel))
+         (spec `((?f . ,filename)
+                 (?r . ,blobname)
+                 (?g . ,toplevel))))
+    (when (and toplevel (file-writable-p toplevel)
+               (not (member blobname
+                            (let ((default-directory filedir))
+                              (magit-git-lines
+                               "ls-files" "--other" "--ignored"
+                               "--exclude-standard" "--full-name")))))
+      (magit-run-git "wip" "save"
+                     (format-spec magit-wip-commit-message spec)
+                     "--editor" "--" filename)
+      (when magit-wip-echo-area-message
+        (message (format-spec magit-wip-echo-area-message spec))))))
+
+(defun magit-wip-mode (&rest ignore)
+  (message "magit-wip-mode is obsolete and doesn't do anything"))
+(make-obsolete 'magit-wip-mode "This mode is a noop now" "2.0.0")
 
 ;;;; Maintenance Tools
 
