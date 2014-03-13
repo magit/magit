magit/magit

commit 482de8051cbb0bba6e9f6bd533097cc7bbdeb7ad

magit-git-repo-p: remove function
This function was problematic because it was very similar to some
related functions but at the same time rather different.  It also
sounded like it did the same as `git rev-parse --resolve-git-dir'
but actually did something very different.  It was also used only
once, so to avoid confusion, remove it.

magit.el
@@ -3453,10 +3453,7 @@ If FILE isn't inside a Git repository then return nil."
       (string-as-multibyte (read path))
     path))
 
-;;;; Repository Predicates
-
-(defun magit-git-repo-p (dir)
-  (file-exists-p (expand-file-name ".git" dir)))
+;;;; Predicates
 
 (defun magit-bare-repo-p ()
   "Return t if the current repository is bare."
@@ -3465,8 +3462,6 @@ If FILE isn't inside a Git repository then return nil."
 (defun magit-no-commit-p ()
   "Return t if there is no commit in the current git repository."
   (not (magit-git-string "rev-list" "-1" "HEAD")))
-
-;;;; Diff Predicates
 
 (defun magit-anything-staged-p ()
   (magit-git-failure "diff" "--quiet" "--cached"))
@@ -7534,7 +7529,8 @@ non-nil, then autocompletion will offer directory names."
 
 (defun magit-list-repos* (dir depth)
   "Return a list of repos found in DIR, recursing up to DEPTH levels deep."
-  (if (magit-git-repo-p dir)
+  (if (magit-git-success "rev-parse" "--resolve-git-dir"
+                         (expand-file-name ".git" dir))
       (list (expand-file-name dir))
     (and (> depth 0)
          (file-directory-p dir)

commit aded8452398ef8322cccceabdda542153525f6c1

magit-list-repos*: bugfix
Fix a bug introduced in 482de8051cbb0bba6e9f6bd533097cc7bbdeb7ad.

magit.el
@@ -7595,8 +7595,7 @@ non-nil, then autocompletion will offer directory names."
 
 (defun magit-list-repos* (dir depth)
   "Return a list of repos found in DIR, recursing up to DEPTH levels deep."
-  (if (magit-git-success "rev-parse" "--resolve-git-dir"
-                         (expand-file-name ".git" dir))
+  (if (file-exists-p (expand-file-name ".git" dir))
       (list (expand-file-name dir))
     (and (> depth 0)
          (file-directory-p dir)