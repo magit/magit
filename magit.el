8505174ea55b93c44b32c9ef196e7b3909a4d348
Jonas Bernoulli <jonas@bernoul.li>
2014-04-09 17:32:00 +0200

    magit-font-lock-keywords: fix for Emacs version 24.4

--- magit.el
+++ magit.el
@@ -7536,7 +7536,7 @@ This command is intended for debugging purposes."
     `((,(concat "(\\(" (regexp-opt
                         '("magit-define-section-jumper"
                           "magit-define-popup"))
-                "\\)\\_>[ \t'\(]*\\(\\sw+\\|\\s_\\)?")
+                "\\)\\_>[ \t'\(]*\\(\\(?:\\sw\\|\\s_\\)+\\)?")
        (1 font-lock-keyword-face)
        (2 font-lock-function-name-face nil t))
       (,(concat "(" (regexp-opt
