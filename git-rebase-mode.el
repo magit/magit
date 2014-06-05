49034fa38dd3a4d1353e1856577a3361c23e7f23
Jonas Bernoulli <jonas@bernoul.li>
2014-06-05 14:20:06 +0200

    bump to 0.15.0

--- git-rebase-mode.el
+++ git-rebase-mode.el
@@ -4,7 +4,6 @@
 
 ;; Author: Phil Jackson <phil@shellarchive.co.uk>
 ;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
-;; Version: 0.14.0
 ;; Homepage: https://github.com/magit/git-modes
 ;; Keywords: convenience vc git
 
