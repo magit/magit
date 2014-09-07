6fcdd82eb2282443ec1038bddb434c7a34797a85
Jonas Bernoulli <jonas@bernoul.li>
2014-09-08 01:28:17 +0200

    define string-prefix-p for older Emacsen
    Fixes #1500.

--- magit-utils.el
+++ magit-utils.el
@@ -111,6 +111,15 @@ BODY."
            (if (functionp ,quit-function)
                (funcall ,quit-function ,window ,value)
              ,value)))))
+
+  ;; Added in Emacs 24.4
+  (unless (fboundp 'string-prefix-p)
+    (defun string-prefix-p (str1 str2 &optional ignore-case)
+      "Return non-nil if STR1 is a prefix of STR2.
+If IGNORE-CASE is non-nil, the comparison is done without paying attention
+to case differences."
+      (eq t (compare-strings str1 nil nil
+                             str2 0 (length str1) ignore-case))))
   )
 
 ;;; User Input
