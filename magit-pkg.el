2f09ca032fc3e6468ef38272374c00aad5e8b618
Michael Griffiths <mikey@cich.li>
2014-12-07 11:59:51 +0000

    Remove merged dependencies from magit-pkg.el

--- magit-pkg.el
+++ magit-pkg.el
@@ -1,7 +1,4 @@
 (define-package "magit" "2.0.50"
   "Control Git from Emacs"
   '((cl-lib "0.5")
-    (dash "2.8.0")
-    (git-commit "1.0.0")
-    (git-rebase "1.0.0")
-    (with-editor "1.0.0")))
+    (dash "2.8.0")))
