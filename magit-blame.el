d2414ff1370e6380111931383161ebc576559078
Jonas Bernoulli <jonas@bernoul.li>
2015-03-11 21:49:50 +0100

    magit-blame-format-heading: work around format-spec limitation
    `format-spec' tries to preserve text properties in
    FORMAT but not in SPECIFICATION. We could use both.

--- magit-blame.el
+++ magit-blame.el
@@ -335,27 +335,36 @@ only arguments available from `magit-blame-popup' should be used.
    'face (list :background (face-attribute 'magit-blame-heading :background))))
 
 (defun magit-blame-format-heading (chunk)
-  (format-spec
-   (propertize (concat magit-blame-heading-format "\n")
-               'face 'magit-blame-heading)
-   `((?H . ,(propertize (or (plist-get chunk :hash) "")
-                        'face 'magit-blame-hash))
-     (?s . ,(propertize (plist-get chunk :summary)
-                        'face 'magit-blame-summary))
-     (?a . ,(propertize (plist-get chunk :author)
-                        'face 'magit-blame-name))
-     (?A . ,(propertize (magit-blame-format-time-string
-                         magit-blame-time-format
-                         (plist-get chunk :author-time)
-                         (plist-get chunk :author-tz))
-                        'face 'magit-blame-date))
-     (?c . ,(propertize (plist-get chunk :committer)
-                        'face 'magit-blame-name))
-     (?C . ,(propertize (magit-blame-format-time-string
-                         magit-blame-time-format
-                         (plist-get chunk :committer-time)
-                         (plist-get chunk :committer-tz))
-                        'face 'magit-blame-date)))))
+  (with-temp-buffer
+    (insert (format-spec
+             (concat magit-blame-heading-format "\n")
+             `((?H . ,(propertize (or (plist-get chunk :hash) "")
+                                  'face 'magit-blame-hash))
+               (?s . ,(propertize (plist-get chunk :summary)
+                                  'face 'magit-blame-summary))
+               (?a . ,(propertize (plist-get chunk :author)
+                                  'face 'magit-blame-name))
+               (?A . ,(propertize (magit-blame-format-time-string
+                                   magit-blame-time-format
+                                   (plist-get chunk :author-time)
+                                   (plist-get chunk :author-tz))
+                                  'face 'magit-blame-date))
+               (?c . ,(propertize (plist-get chunk :committer)
+                                  'face 'magit-blame-name))
+               (?C . ,(propertize (magit-blame-format-time-string
+                                   magit-blame-time-format
+                                   (plist-get chunk :committer-time)
+                                   (plist-get chunk :committer-tz))
+                                  'face 'magit-blame-date)))))
+    (goto-char (point-min))
+    (while (not (eobp))
+      (let ((face (get-text-property (point) 'face))
+            (next (or (next-single-property-change (point) 'face)
+                      (point-max))))
+        (unless face
+          (put-text-property (point) next 'face 'magit-blame-heading))
+        (goto-char next)))
+    (buffer-string)))
 
 (defun magit-blame-format-time-string (format time tz)
   (format-time-string
