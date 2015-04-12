93ce697d14503921d14c0b707512be1c50b29f86
Jonas Bernoulli <jonas@bernoul.li>
2015-04-12 02:07:01 +0200

    no longer support the obsolete magit-log-edit library
    and make sure that those who still have it installed remove it.
    This will cause their committing experience to get much nicer.

--- magit.el
+++ magit.el
@@ -65,12 +65,7 @@ Use the function by the same name instead of this variable.")
 (when (version< emacs-version "23.2")
   (error "Magit requires at least GNU Emacs 23.2"))
 
-;; Users may choose to use `magit-log-edit' instead of the preferred
-;; `git-commit-mode', by simply putting it on the `load-path'.  If
-;; it can be found there then it is loaded at the end of this file.
-(unless (locate-library "magit-log-edit")
-  (require 'git-commit-mode))
-
+(require 'git-commit-mode)
 (require 'git-rebase-mode)
 
 (require 'ansi-color)
@@ -7813,7 +7808,17 @@ Emacs:
   (setq magit-last-seen-setup-instructions \"1.4.0\")
 
 You might also want to read the release notes:
-https://raw.githubusercontent.com/magit/magit/next/Documentation/RelNotes/1.4.0.txt")))
+https://raw.githubusercontent.com/magit/magit/next/Documentation/RelNotes/1.4.0.txt"))
+  (when (featurep 'magit-log-edit)
+    (display-warning :error "magit-log-edit has to be removed
+
+Magit is no longer compatible with the library `magit-log-edit',
+which was used in earlier releases.  Please remove it, so that
+Magit can use the successor `git-commit-mode' without the
+obsolete library getting in the way.  Then restart Emacs.
+
+You might also want to read:
+https://github.com/magit/magit/wiki/Emacsclient")))
 
 (add-hook 'after-init-hook #'magit-maybe-show-setup-instructions)
 
@@ -7831,14 +7836,7 @@ https://raw.githubusercontent.com/magit/magit/next/Documentation/RelNotes/1.4.0.
 
 (provide 'magit)
 
-;; rest of magit core
 (require 'magit-key-mode)
-
-;; If `magit-log-edit' is available and `git-commit-mode' is not
-;; loaded, then we have no choice but to assume the user actually
-;; wants to use the former.
-(unless (featurep 'git-commit-mode)
-  (require 'magit-log-edit nil t))
 
 ;; Local Variables:
 ;; coding: utf-8
