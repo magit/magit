e07db2e6ae942ac2eab8975c2f92379b2fe24d2f
Jonas Bernoulli <jonas@bernoul.li>
2015-05-10 01:13:25 +0200

    magit-revert-buffers: don't use auto-revert-handler
    `auto-revert-handler' is only intended for internal use and indeed a
    change to it broke our use of it.  (But I think that change might be
    a bug).  Instead copy the small parts of that function which are
    actually relevant to us into `magit-revert-buffers'.
    
    Fixes #1821 and #1844.

--- magit.el
+++ magit.el
@@ -4065,8 +4065,13 @@ the current repository."
               (and file (string-prefix-p topdir file)
                    (not (string-prefix-p gitdir file))
                    (member (file-relative-name file topdir) tracked)
-                   (let ((auto-revert-mode t))
-                     (auto-revert-handler)
+                   (let ((remote-file-name-inhibit-cache t))
+                     (setq auto-revert-notify-modified-p nil)
+                     (when auto-revert-verbose
+                       (message "Reverting buffer `%s'." (buffer-name)))
+                     (let ((buffer-read-only buffer-read-only))
+                       (revert-buffer 'ignore-auto 'dont-ask 'preserve-modes))
+                     (vc-find-file-hook)
                      (run-hooks 'magit-revert-buffer-hook))))))))))
 
 ;;; (misplaced)
