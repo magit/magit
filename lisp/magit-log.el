magit/magit

commit a8838e5e1f59398e8c43e0756baedded47d011a5

use magit-file-relative-name when appropriate

lisp/magit-log.el
@@ -544,8 +544,7 @@ completion candidates."
 With a prefix argument or when `--follow' is part of
 `magit-log-arguments', then follow renames."
   (interactive "P")
-  (-if-let (file (or (buffer-file-name (buffer-base-buffer))
-                     magit-buffer-file-name))
+  (-if-let (file (magit-file-relative-name))
       (magit-mode-setup magit-log-buffer-name-format nil
                         #'magit-log-mode
                         #'magit-log-refresh-buffer
@@ -556,7 +555,7 @@ With a prefix argument or when `--follow' is part of
                               (cons "--follow" args)
                             args))
                         (list (file-relative-name file (magit-toplevel))))
-    (user-error "Buffer isn't visiting a file"))
+    (user-error "Buffer isn't visiting a blob or a file in a Git repository"))
   (magit-log-goto-same-commit))
 
 ;;;###autoload

lisp/magit-commit.el
@@ -334,7 +334,7 @@ actually insert the entry."
       (with-current-buffer buf
         (goto-char pos)
         (funcall magit-commit-add-log-insert-function log
-                 (file-relative-name buffer-file-name (magit-toplevel))
+                 (magit-file-relative-name)
                  (add-log-current-defun))))))
 
 (defun magit-commit-add-log-insert (buffer file defun)

commit 5548927abdc5b3e8ce3190cc53dd93dd6ba049fd

magit-log-buffer-file: fix subdirectory paths
Path adjustment was broken by a8838e5 (use magit-file-relative-name when
appropriate, 2015-09-05),

Fixes #2254.

lisp/magit-log.el
@@ -554,7 +554,7 @@ With a prefix argument or when `--follow' is part of
                           (if (and follow (not (member "--follow" args)))
                               (cons "--follow" args)
                             args))
-                        (list (file-relative-name file (magit-toplevel))))
+                        (list file))
     (user-error "Buffer isn't visiting a file"))
   (magit-log-goto-same-commit))
 