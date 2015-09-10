023a163d34fe079c02abc0fde05161dfa1fa7f60
Jonas Bernoulli <jonas@bernoul.li>
2015-09-10 19:11:14 +0200

    magit-log-maybe-show-commit: or blob
    If `blob-follow' is a member of `magit-diff-auto-show' and another
    buffer displays a blob, then update that like revision buffer would
    be updated when `log-follow' is a member of that option.
    
    If a blob buffer is updated, then no attempt is made to also update
    the revision buffer.

--- lisp/magit-log.el
+++ lisp/magit-log.el
@@ -33,6 +33,7 @@
 (require 'magit-diff)
 
 (declare-function magit-blame-chunk-get 'magit-blame)
+(declare-function magit-find-file-noselect 'magit)
 (declare-function magit-insert-head-header 'magit)
 (declare-function magit-insert-upstream-header 'magit)
 (declare-function magit-read-file-from-rev 'magit)
@@ -964,14 +965,26 @@ another window, using `magit-show-commit'."
            magit-diff-auto-show-delay nil
            (lambda ()
              (magit-section-when commit
-               (when (or (and (magit-diff-auto-show-p 'log-follow)
-                              (magit-mode-get-buffer
-                               nil 'magit-revision-mode nil nil t))
-                         (and (magit-diff-auto-show-p 'log-oneline)
-                              (derived-mode-p 'magit-log-mode)))
-                 (apply #'magit-show-commit
-                        (magit-section-value it) t nil
-                        (magit-diff-arguments))))
+               (let ((rev (magit-section-value it)))
+                 (--if-let (and (magit-diff-auto-show-p 'blob-follow)
+                                (derived-mode-p 'magit-log-mode)
+                                (--first (with-current-buffer it
+                                           magit-buffer-revision)
+                                         (-map #'window-buffer (window-list))))
+                     (save-excursion
+                       (with-selected-window (get-buffer-window it)
+                         (with-current-buffer it
+                           (magit-blob-visit (list (magit-rev-parse rev)
+                                                   (magit-file-relative-name
+                                                    magit-buffer-file-name))
+                                             (line-number-at-pos)))))
+                   (when (or (and (magit-diff-auto-show-p 'log-follow)
+                                  (magit-mode-get-buffer
+                                   nil 'magit-revision-mode nil nil t))
+                             (and (magit-diff-auto-show-p 'log-oneline)
+                                  (derived-mode-p 'magit-log-mode)))
+                     (apply #'magit-show-commit rev t nil
+                            (magit-diff-arguments))))))
              (setq magit-update-other-window-timer nil))))))
 
 (defun magit-log-goto-same-commit ()
