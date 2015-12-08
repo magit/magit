magit/magit

commit bdcd61f044d911187a85d9f42434d6eb614d2fbb

magit-gitignore-locally: drop unused argument
And adjust `magit-gitignore-read-args' accordingly.

lisp/magit-extras.el
@@ -138,9 +138,8 @@ With a prefix argument only ignore locally."
       (magit-run-git "add" ".gitignore"))))
 
 ;;;###autoload
-(defun magit-gitignore-locally (file-or-pattern &optional local)
-  "Instruct Git to locally ignore FILE-OR-PATTERN.
-\n(fn FILE-OR-PATTERN)"
+(defun magit-gitignore-locally (file-or-pattern)
+  "Instruct Git to locally ignore FILE-OR-PATTERN."
   (interactive (magit-gitignore-read-args t))
   (magit-gitignore file-or-pattern t))
 
@@ -160,10 +159,11 @@ With a prefix argument only ignore locally."
         (setq default (concat "*." (file-name-extension default)))
         (unless (member default choices)
           (setq default nil))))
-    (list (magit-completing-read
-           (concat "File or pattern to ignore" (and local " locally"))
-           choices nil nil nil nil default)
-          local)))
+    (let ((file-or-pattern
+           (magit-completing-read (concat "File or pattern to ignore"
+                                          (and local " locally"))
+                                  choices nil nil nil nil default)))
+      (if local (list file-or-pattern t) file-or-pattern))))
 
 ;;; ChangeLog
 

commit f2d7fc326d7cd05eda44b8e256217fc6ca652aea

magit-gitignore{,-locally}: fix argument processing
Changes in bdcd61f (magit-gitignore-locally: drop unused argument,
2015-12-08) introduced two errors:

* magit-gitignore recieves a string rather than a list from
  magit-gitignore-read-args when LOCAL is nil, resulting in a type
  error.

* magit-gitignore-locally failed due to a wrong number of arguments.

magit-gitignore and magit-gitignore-locally now take a different number
of arguments, so convert magit-gitignore-read-args into a function that
is only responsible for returning the pattern and let the commands deal
with constructing the proper arguments.

lisp/magit-extras.el
@@ -118,7 +118,8 @@ with two prefix arguments remove ignored files only.
 (defun magit-gitignore (file-or-pattern &optional local)
   "Instruct Git to ignore FILE-OR-PATTERN.
 With a prefix argument only ignore locally."
-  (interactive (magit-gitignore-read-args current-prefix-arg))
+  (interactive (list (magit-gitignore-read-pattern current-prefix-arg)
+                     current-prefix-arg))
   (let ((gitignore
          (if local
              (magit-git-dir (convert-standard-filename "info/exclude"))
@@ -140,10 +141,10 @@ With a prefix argument only ignore locally."
 ;;;###autoload
 (defun magit-gitignore-locally (file-or-pattern)
   "Instruct Git to locally ignore FILE-OR-PATTERN."
-  (interactive (magit-gitignore-read-args t))
+  (interactive (list (magit-gitignore-read-pattern t)))
   (magit-gitignore file-or-pattern t))
 
-(defun magit-gitignore-read-args (local)
+(defun magit-gitignore-read-pattern (local)
   (let* ((default (magit-current-file))
          (choices
           (delete-dups
@@ -159,11 +160,9 @@ With a prefix argument only ignore locally."
         (setq default (concat "*." (file-name-extension default)))
         (unless (member default choices)
           (setq default nil))))
-    (let ((file-or-pattern
-           (magit-completing-read (concat "File or pattern to ignore"
-                                          (and local " locally"))
-                                  choices nil nil nil nil default)))
-      (if local (list file-or-pattern t) file-or-pattern))))
+    (magit-completing-read (concat "File or pattern to ignore"
+                                   (and local " locally"))
+                           choices nil nil nil nil default)))
 
 ;;; ChangeLog
 