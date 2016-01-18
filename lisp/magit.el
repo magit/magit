954d2d55ab6f644cf8a9a20be3287343f950682e
Kyle Meyer <kyle@kyleam.com>
2016-01-18 00:29:13 -0500

    fix 4394b7e352efe5dec43cf67283bdc23ac59a63e4

--- lisp/magit.el
+++ lisp/magit.el
@@ -16,7 +16,7 @@
 ;;	Rémi Vanicat      <vanicat@debian.org>
 ;;	Yann Hodique      <yann.hodique@gmail.com>
 
-;; Package-Requires: ((emacs "24.4") (async "20150909.2257") (dash "20151021.113") (with-editor "20160117.1513") (git-commit "20160118") (magit-popup "20160117.1513"))
+;; Package-Requires: ((emacs "24.4") (async "20150909.2257") (dash "20151021.113") (with-editor "20160117.1513") (git-commit "20160117.1513") (magit-popup "20160117.1513"))
 ;; Keywords: git tools vc
 ;; Homepage: https://github.com/magit/magit
 
