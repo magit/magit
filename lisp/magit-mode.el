0c3e8fa13fa2120a8e541f17834c2605a8ece78a
Jonas Bernoulli <jonas@bernoul.li>
2016-01-27 15:36:39 +0100

    magit-refresh-file-visiting-buffers-hook: new hook

--- lisp/magit-mode.el
+++ lisp/magit-mode.el
@@ -681,11 +681,14 @@ window."
 
 (defvar inhibit-magit-refresh nil)
 
-(defun magit-refresh ()
+(defun magit-refresh (&optional head)
   "Refresh some buffers belonging to the current repository.
 
 Refresh the current buffer if its major mode derives from
-`magit-mode', and refresh the corresponding status buffer."
+`magit-mode', and refresh the corresponding status buffer.
+
+Run hooks `magit-pre-refresh-hook', `magit-post-refresh-hook',
+and `magit-refresh-file-visiting-buffers-hook'."
   (interactive)
   (unless inhibit-magit-refresh
     (magit-run-hook-with-benchmark 'magit-pre-refresh-hook)
@@ -697,8 +700,10 @@ Refresh the current buffer if its major mode derives from
       (with-current-buffer it
         (magit-refresh-buffer)))
     (magit-auto-revert-buffers)
+    (magit-refresh-file-visiting-buffers head)
     (magit-run-hook-with-benchmark 'magit-post-refresh-hook)))
 
+;; TODO run other hooks too
 (defun magit-refresh-all ()
   "Refresh all buffers belonging to the current repository.
 
@@ -709,7 +714,8 @@ tracked in the current repository."
   (interactive)
   (dolist (buffer (magit-mode-get-buffers))
     (with-current-buffer buffer (magit-refresh-buffer)))
-  (magit-auto-revert-buffers))
+  (magit-auto-revert-buffers)
+  (magit-refresh-file-visiting-buffers "force"))
 
 (defvar-local magit-refresh-start-time nil)
 
@@ -771,6 +777,39 @@ tracked in the current repository."
                                  (regexp-quote (buffer-substring-no-properties
                                                 beg (line-end-position))))
                                 (t t)))))))))
+
+;;; Refresh File-Visiting Buffers
+
+(defun magit-refresh-file-visiting-buffers (head)
+  (let (start topdir)
+    (when (and magit-refresh-file-visiting-buffers-hook
+               (setq topdir (magit-toplevel)))
+      (with-local-quit
+        (when magit-refresh-verbose
+          (setq start (current-time))
+          (message "Running magit-refresh-file-visiting-buffers-hook..."))
+        (run-hook-with-args
+         'magit-refresh-file-visiting-buffers-hook
+         (if (member head (list nil (magit-rev-parse "HEAD")))
+             (--keep (find-buffer-visiting (expand-file-name it topdir))
+                     (magit-modified-files))
+           (let ((gitdir (magit-git-dir)))
+             (--filter
+              (with-current-buffer it
+                (and buffer-file-name
+                     (file-in-directory-p buffer-file-name topdir)
+                     (not (file-in-directory-p buffer-file-name gitdir))))
+              (buffer-list)))))
+        (when magit-refresh-verbose
+          (message "Running %s...done (%.3fs)"
+                   'magit-refresh-file-visiting-buffers-hook
+                   (float-time (time-subtract (current-time) start))))))))
+
+(add-hook 'magit-refresh-file-visiting-buffers-hook 'diff-hl-update-buffers)
+(defun diff-hl-update-buffers (buffers)
+  (dolist (buf buffers)
+    (with-current-buffer buf
+      (diff-hl-update))))
 
 ;;; Save File-Visiting Buffers
 
