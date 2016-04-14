c4e6297dbf5024992865824df0fad3a71ad13425
Jonas Bernoulli <jonas@bernoul.li>
2016-04-14 11:50:09 +0200

    release version 2.6.1

--- lisp/magit.el
+++ lisp/magit.el
@@ -16,7 +16,7 @@
 ;;	Rémi Vanicat      <vanicat@debian.org>
 ;;	Yann Hodique      <yann.hodique@gmail.com>
 
-;; Package-Requires: ((emacs "24.4") (async "20150909.2257") (dash "20151021.113") (with-editor "20160223.115") (git-commit "20160130.64") (magit-popup "20160302.322"))
+;; Package-Requires: ((emacs "24.4") (async "1.5") (dash "2.12.1") (with-editor "2.5.0") (git-commit "2.6.0") (magit-popup "2.6.0"))
 ;; Keywords: git tools vc
 ;; Homepage: https://github.com/magit/magit
 
