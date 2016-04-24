1821e46727e5124c4679077c2bd47ec0e425747f
Jonas Bernoulli <jonas@bernoul.li>
2016-04-24 17:09:00 +0200

    magit-submodule-add: reuse the already configure name
    If one adds a submodule and then (potentially accidentally) unstaged the
    submodule again, then the changes to .gitsubmodules persist.  When later
    re-adding the repository `magit-submodule-add' now offers the already
    configure submodule name as default choice.

--- lisp/magit-submodule.el
+++ lisp/magit-submodule.el
@@ -68,7 +68,12 @@ PATH also becomes the name."
                                         (expand-file-name path))))
                 (magit-get "remote" (or (magit-get-remote) "origin") "url")))
              (directory-file-name path)
-             (magit-read-string-ns "Name submodule" path)))))
+             (magit-read-string-ns
+              "Name submodule" nil nil
+              (--keep (-let [(var val) (split-string it "=")]
+                        (and (equal val path)
+                             (cadr (split-string var "\\."))))
+                      (magit-git-lines "config" "--list" "-f" ".gitmodules")))))))
   (magit-run-git "submodule" "add" (and name (list "--name" name)) url path))
 
 ;;;###autoload
