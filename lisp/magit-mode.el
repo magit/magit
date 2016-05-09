1c28c17f29f35cbadaa87bbc58f677e0ead24763
Jonas Bernoulli <jonas@bernoul.li>
2016-05-10 01:05:51 +0200

    magit-display-buffer-traditional: limit use of kludge
    This fixes the kludge from a55baa8ab61d23367a2c2978be790e016afb1438.
    Also see #2632.

--- lisp/magit-mode.el
+++ lisp/magit-mode.el
@@ -516,15 +516,17 @@ and `magit-post-display-buffer-hook'."
 (defun magit-display-buffer-traditional (buffer)
   "Display BUFFER the way this has traditionally been done."
   (display-buffer
-   buffer (if (and (derived-mode-p 'magit-mode)
-                   (not (memq (with-current-buffer buffer major-mode)
-                              '(magit-process-mode
-                                magit-revision-mode
-                                magit-diff-mode
-                                magit-stash-mode
-                                magit-status-mode))))
-              '(display-buffer-same-window)
-            '(display-buffer-use-some-window . (inhibit-same-window t)))))
+   buffer (cond ((and (derived-mode-p 'magit-mode)
+                      (not (memq (with-current-buffer buffer major-mode)
+                                 '(magit-process-mode
+                                   magit-revision-mode
+                                   magit-diff-mode
+                                   magit-stash-mode
+                                   magit-status-mode))))
+                 '(display-buffer-same-window))
+                (with-editor-mode ; see #2632
+                 '(display-buffer-use-some-window . (inhibit-same-window t)))
+                (t nil)))) ; display in another window
 
 (defun magit-maybe-set-dedicated ()
   "Mark the selected window as dedicated if appropriate.
