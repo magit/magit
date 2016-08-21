0dcebfe75e8746ce26bda650b45d476e4cdb0b1b
Jonas Bernoulli <jonas@bernoul.li>
2016-08-21 22:38:30 +0200

    reset Package-Requires for Melpa

--- lisp/magit.el
+++ lisp/magit.el
@@ -16,7 +16,7 @@
 ;;	Rémi Vanicat      <vanicat@debian.org>
 ;;	Yann Hodique      <yann.hodique@gmail.com>
 
-;; Package-Requires: ((emacs "24.4") (async "1.9") (dash "2.13.0") (with-editor "2.5.1") (git-commit "2.7.0") (magit-popup "2.7.0"))
+;; Package-Requires: ((emacs "24.4") (async "20160711.223") (dash "20160820.501") (with-editor "20160812.1457") (git-commit "20160519.950") (magit-popup "20160813.642"))
 ;; Keywords: git tools vc
 ;; Homepage: https://github.com/magit/magit
 
