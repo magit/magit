eb6d8bde3f460979fe18eb9f122734d86fd552fa
Jonas Bernoulli <jonas@bernoul.li>
2016-12-22 13:25:00 +0100

    magit-update-other-window-delay: fix custom type

--- lisp/magit-utils.el
+++ lisp/magit-utils.el
@@ -271,7 +271,7 @@ and delay of your graphical environment or operating system."
 (defcustom magit-view-git-manual-method 'info
   "How links to Git documentation are followed from Magit's Info manuals.
 
-`nil'   Follow the link to the node in the `gitman' Info manual
+`info'  Follow the link to the node in the `gitman' Info manual
         as usual.  Unfortunately that manual is not installed by
         default on some platforms, and when it is then the nodes
         look worse than the actual manpages.
@@ -281,7 +281,7 @@ and delay of your graphical environment or operating system."
 `woman' View the respective man-page using the `woman' package."
   :package-version '(magit . "2.9.0")
   :group 'magit-modes
-  :type '(choice (const :tag "view info manual" nil)
+  :type '(choice (const :tag "view info manual" info)
                  (const :tag "view manpage using `man'" man)
                  (const :tag "view manpage using `woman'" woman)))
 
