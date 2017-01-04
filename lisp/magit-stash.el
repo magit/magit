f00dedd62be2eb91f235dd9b9d18cee924fde5c9
Kyle Meyer <kyle@kyleam.com>
2017-01-04 18:03:59 -0500

    magit-stash-drop: abort if multi-stash deletion is not confirmed
    Fixes #2917.

--- lisp/magit-stash.el
+++ lisp/magit-stash.el
@@ -214,7 +214,8 @@ and forgo removing the stash."
   "Remove a stash from the stash list.
 When the region is active offer to drop all contained stashes."
   (interactive (list (--if-let (magit-region-values 'stash)
-                         (magit-confirm t nil "Drop %i stashes" it)
+                         (or (magit-confirm t nil "Drop %i stashes" it)
+                             (user-error "Abort"))
                        (magit-read-stash "Drop stash"))))
   (dolist (stash (if (listp stash)
                      (nreverse (prog1 stash (setq stash (car stash))))
