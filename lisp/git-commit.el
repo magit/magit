magit/magit

commit 084182e8e7dc82550543a4933c082a2a188d7faf

git-commit-setup: use normal-mode
This ensures file and directory local variables are setup in the
standard way.

lisp/git-commit.el
@@ -391,7 +391,11 @@ to consider doing so."
     (when (file-accessible-directory-p (file-name-directory it))
       (find-alternate-file it)))
   (when git-commit-major-mode
-    (funcall git-commit-major-mode))
+    (let ((auto-mode-alist (list (cons (concat "\\`"
+                                               (regexp-quote buffer-file-name)
+                                               "\\'")
+                                       git-commit-major-mode))))
+      (normal-mode)))
   (setq with-editor-show-usage nil)
   (with-editor-mode 1)
   (add-hook 'with-editor-finish-query-functions

commit c6624d143aa3c3e210a04d15e56f42c156226198

git-commit-setup: Call 'normal-mode' with argument
This fixes a regression introduced by commit
084182e8e7dc82550543a4933c082a2a188d7faf.

According to the docstring (and the code) of 'normal-mode', when it is
called without argument, 'enable-local-variables' is ignored, so if
there are unsafe local variables, a user will be prompted about them
every time.

lisp/git-commit.el
@@ -407,7 +407,7 @@ already using it, then you probably shouldn't start doing so."
                                                (regexp-quote buffer-file-name)
                                                "\\'")
                                        git-commit-major-mode))))
-      (normal-mode)))
+      (normal-mode t)))
   (setq with-editor-show-usage nil)
   (with-editor-mode 1)
   (add-hook 'with-editor-finish-query-functions