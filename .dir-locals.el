cd2a9b8c5ac90c2cc7f945c9fd9b1e7884b9ee91
Jonas Bernoulli <jonas@bernoul.li>
2017-01-12 17:01:01 +0100

    Set file-local value of outline-regexp for makefiles

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,5 @@
 ((nil
   (bug-reference-bug-regexp . "#\\(?2:[0-9]+\\)")
-  (bug-reference-url-format . "https://github.com/magit/magit/issues/%s")))
+  (bug-reference-url-format . "https://github.com/magit/magit/issues/%s"))
+ (makefile-gmake-mode
+  (outline-regexp . "##")))
