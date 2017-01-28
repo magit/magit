acba806a823977108bae60438466da71f773a7c8
Jonas Bernoulli <jonas@bernoul.li>
2017-01-28 16:44:44 +0100

    Release version 2.10.1

--- lisp/git-commit.el
+++ lisp/git-commit.el
@@ -11,7 +11,7 @@
 ;;	Marius Vollmer <marius.vollmer@gmail.com>
 ;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
 
-;; Package-Requires: ((emacs "24.4") (dash "20161121.55") (with-editor "20161231.826"))
+;; Package-Requires: ((emacs "24.4") (dash "2.13.0") (with-editor "2.5.9"))
 ;; Keywords: git tools vc
 ;; Homepage: https://github.com/magit/magit
 
