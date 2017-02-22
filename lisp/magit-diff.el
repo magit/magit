297930e319e85cf99c1a905e36be4cdb3cd05999
Basil L. Contovounesios <contovob@tcd.ie>
2017-02-22 02:04:23 +0000

    doc fix: magit-diff highlight variable names

--- lisp/magit-diff.el
+++ lisp/magit-diff.el
@@ -169,9 +169,10 @@ t      show fine differences for the current diff hunk only.
 
 (defcustom magit-diff-paint-whitespace t
   "Specify where to highlight whitespace errors.
-See `magit-highlight-trailing-whitespace',
-`magit-highlight-indentation'.  The symbol t means in all diffs,
-`status' means only in the status buffer, and nil means nowhere."
+See `magit-diff-highlight-trailing',
+`magit-diff-highlight-indentation'.  The symbol t means in all
+diffs, `status' means only in the status buffer, and nil means
+nowhere."
   :group 'magit-diff
   :safe (lambda (val) (memq val '(t nil status)))
   :type '(choice (const :tag "Always" t)
