cafd209ce7f4d9da4b483f29da2ea1e9ebd8b44c
Jonas Bernoulli <jonas@bernoul.li>
2017-03-17 15:15:41 +0100

    Patch vc-git-conflicted-files to work around debug#21559
    VC adds a hook to auto-revert for files under Git that checks whether
    the current file has a merge conflict and enables smerge-mode.  This
    check grabs the index lock.  When a rebase is done, Git releases the
    index lock in between each cherry-pick.  This gives auto-revert a
    chance to steal the lock, which causes the next operation in the
    rebase to fail.
    
    The lock is grabbed by `git status --porcelain --' run in this
    function.  We change it to use `git diff-files --name-status' instead,
    which does not grab the lock.
    
    Also see the preceding commit, which imported the unpatched upstream
    implementation of this function.
    
    Re #2708 and https://debbugs.gnu.org/cgi/bugreport.cgi?bug=21559.
    
    Thanks to Jason Merrill for the patch and Allen for pointing me toward
    it.

--- lisp/magit-utils.el
+++ lisp/magit-utils.el
@@ -560,20 +560,15 @@ and https://github.com/magit/magit/issues/2295."
     (defun vc-git-conflicted-files (directory)
       "Return the list of files with conflicts in DIRECTORY."
       (let* ((status
-              (vc-git--run-command-string directory "status" "--porcelain" "--"))
+              (vc-git--run-command-string directory "diff-files"
+                                          "--name-status"))
              (lines (when status (split-string status "\n" 'omit-nulls)))
              files)
-        ;; TODO: Look into reimplementing `vc-git-state', as well as
-        ;; `vc-git-dir-status-files', based on this output, thus making the
-        ;; extra process call in `vc-git-find-file-hook' unnecessary.
         (dolist (line lines files)
-          (when (string-match "\\([ MADRCU?!][ MADRCU?!]\\) \\(.+\\)\\(?: -> \\(.+\\)\\)?"
-                              line)
+          (when (string-match "\\([ MADRCU?!]\\)[ \t]+\\(.+\\)" line)
             (let ((state (match-string 1 line))
                   (file (match-string 2 line)))
-              ;; See git-status(1).
-              (when (member state '("AU" "UD" "UA" ;; "DD"
-                                    "DU" "AA" "UU"))
+              (when (equal state "U")
                 (push (expand-file-name file directory) files)))))))))
 
 ;;; Kludges for Incompatible Modes
