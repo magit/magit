56756c81f1c79e13bbfc396a165c7d740e2a7d99
Jonas Bernoulli <jonas@bernoul.li>
2017-04-20 13:16:27 +0200

    magit-blame-disabled-modes: declare properly

--- lisp/magit-blame.el
+++ lisp/magit-blame.el
@@ -73,8 +73,6 @@ and then turned on again when turning off the latter."
   :group 'magit-blame
   :type '(repeat (symbol :tag "Mode")))
 
-(make-variable-buffer-local 'magit-blame-disabled-modes)
-
 (defcustom magit-blame-mode-lighter " Blame"
   "The mode-line lighter of the Magit-Blame mode."
   :group 'magit-blame
@@ -155,6 +153,7 @@ and then turned on again when turning off the latter."
 
 (defvar-local magit-blame-buffer-read-only nil)
 (defvar-local magit-blame-cache nil)
+(defvar-local magit-blame-disabled-modes nil)
 (defvar-local magit-blame-process nil)
 (defvar-local magit-blame-recursive-p nil)
 (defvar-local magit-blame-separator nil)
