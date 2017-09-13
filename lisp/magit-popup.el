e95cb2c444c8b4300870d94d81e30f34c401c1c9
Jonas Bernoulli <jonas@bernoul.li>
2017-09-13 12:43:38 +0200

    Reset Package-Requires for Melpa

--- lisp/magit-popup.el
+++ lisp/magit-popup.el
@@ -12,7 +12,7 @@
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Maintainer: Jonas Bernoulli <jonas@bernoul.li>
 
-;; Package-Requires: ((emacs "24.4") (async "1.9.2") (dash "2.13.0"))
+;; Package-Requires: ((emacs "24.4") (async "20170823") (dash "20170810"))
 ;; Keywords: bindings
 ;; Homepage: https://github.com/magit/magit
 
--- lisp/magit.el
+++ lisp/magit.el
@@ -16,7 +16,7 @@
 ;;	Rémi Vanicat      <vanicat@debian.org>
 ;;	Yann Hodique      <yann.hodique@gmail.com>
 
-;; Package-Requires: ((emacs "24.4") (async "1.9.2") (dash "2.13.0") (with-editor "2.6.0") (git-commit "2.10.3") (magit-popup "2.10.3"))
+;; Package-Requires: ((emacs "24.4") (async "20170823") (dash "20170810") (with-editor "20170817") (git-commit "20170823") (magit-popup "20170824"))
 ;; Keywords: git tools vc
 ;; Homepage: https://github.com/magit/magit
 
