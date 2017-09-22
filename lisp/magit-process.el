7a1f1eb44f86534e022c051f0cd7c22b4602449d
Noam Postavsky <npostavs@users.sourceforge.net>
2017-09-22 19:13:19 -0400

    magit-run-git: Delete cache after running "init" or "clone"

--- lisp/magit-process.el
+++ lisp/magit-process.el
@@ -292,6 +292,9 @@ Process output goes into a new section in the buffer returned by
 `magit-process-buffer'."
   (let ((magit--refresh-cache (list (cons 0 0))))
     (magit-call-git args)
+    (when (member (car args) '("init" "clone"))
+      ;; Creating a new repository invalidates the cache.
+      (setq magit--refresh-cache nil))
     (magit-refresh)))
 
 (defvar magit-pre-call-git-hook nil)
