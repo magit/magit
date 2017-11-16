57e2f2508cd7a0fe4b149650a959cbf07fc1fab1
Jonas Bernoulli <jonas@bernoul.li>
2017-11-16 13:27:51 +0100

    magit-blame-popup: fix magit-blame-reverse's description

--- lisp/magit-blame.el
+++ lisp/magit-blame.el
@@ -213,7 +213,7 @@ and then turned on again when turning off the latter."
   :options  '((?M "Detect lines moved or copied within a file" "-M")
               (?C "Detect lines moved or copied between files" "-C"))
   :actions  '((?b "Show blob touching these lines" magit-blame)
-              (?f "Show blob removing these lines" magit-blame-reverse))
+              (?f "Show last blob with these lines" magit-blame-reverse))
   :default-arguments '("-w")
   :max-action-columns 1
   :default-action 'magit-blame)
