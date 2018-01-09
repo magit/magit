187c56a4e329f62814f3877e97c965d0ec8c95ac
Jonas Bernoulli <jonas@bernoul.li>
2018-01-09 18:04:25 +0100

    Silence byte-compiler

--- lisp/magit-mode.el
+++ lisp/magit-mode.el
@@ -43,6 +43,8 @@
 (defvar magit-revision-show-xref-buttons)
 ;; For `magit-refresh' and `magit-refresh-all'
 (declare-function magit-auto-revert-buffers 'magit-autorevert)
+;; For `magit-refresh-buffer'
+(declare-function magit-process-unset-mode-line-error-status 'magit-process)
 
 (require 'format-spec)
 (require 'help-mode)
