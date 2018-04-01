cabecb46f66aad10495a9e21bb37bb643245c8b9
Jonas Bernoulli <jonas@bernoul.li>
2018-04-01 12:14:01 +0200

    Reset Package-Requires for Melpa

--- lisp/magit-pkg.el
+++ lisp/magit-pkg.el
@@ -1,10 +1,10 @@
 (define-package "magit" "2.12.0"
   "A Git porcelain inside Emacs."
   '((emacs "24.4")
-    (async "1.9.2")
-    (dash "2.13.0")
-    (ghub "2.0.0")
-    (git-commit "2.12.0")
+    (async "20170823")
+    (dash "20170810")
+    (ghub "20180328")
+    (git-commit "20180329")
     (let-alist "1.0.5")
-    (magit-popup "2.12.3")
-    (with-editor "2.7.2")))
+    (magit-popup "20180329")
+    (with-editor "20180318")))
