c6d51ae18edf9e7af85ead8fecef894940f515f2
Jonas Bernoulli <jonas@bernoul.li>
2018-04-06 20:10:52 +0200

    magit-branch-pull-request: For upstream prs adjust upstream too
    When creating a local branch L for a branch R from the upstream
    repository, then set L's upstream to the base branch instead of
    to R, like for branches from forks.
    
    Closes #3417.

--- lisp/magit-branch.el
+++ lisp/magit-branch.el
@@ -413,18 +413,17 @@ Please see the manual for more information."
                  ((string-prefix-p "git://" upstream-url)
                   .head.repo.git_url)
                  (t (error "%s has an unexpected format" upstream-url)))))
-        (magit-call-git "branch" branch
-                        (concat remote "/" pr-branch))
-        (magit-call-git "branch" branch
-                        (concat "--set-upstream-to="
-                                (if magit-branch-prefer-remote-upstream
-                                    (concat upstream "/" .base.ref)
-                                  .base.ref)))
+        (magit-call-git "branch" branch (concat remote "/" pr-branch))
         (magit-set "true" "branch" branch "rebase")
         (if (or .locked (not (equal branch pr-branch)))
             (magit-set upstream "branch" branch "pushRemote")
           (magit-set remote "branch" branch "pushRemote"))
         (magit-set remote "branch" branch "pullRequestRemote"))
+      (magit-call-git "branch" branch
+                      (concat "--set-upstream-to="
+                              (if magit-branch-prefer-remote-upstream
+                                  (concat upstream "/" .base.ref)
+                                .base.ref)))
       (magit-set (number-to-string .number) "branch" branch "pullRequest")
       (magit-set .title                     "branch" branch "description")
       (magit-refresh)
