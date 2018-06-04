494086c60efeca2067b504e883499c865b547f4d
Jonas Bernoulli <jonas@bernoul.li>
2018-06-04 11:53:19 -0500

    Update Package-Requires

--- lisp/magit-pkg.el
+++ lisp/magit-pkg.el
@@ -4,7 +4,7 @@
     (async "20180527")
     (dash "20180413")
     (ghub "20180417")
-    (git-commit "20180411")
+    (git-commit "20180602")
     (let-alist "1.0.5")
     (magit-popup "20180509")
     (with-editor "20180414")))
