aba4bdaf95ce944bb37c6851b5005941c9d15842
Jonas Bernoulli <jonas@bernoul.li>
2018-06-04 12:07:59 -0500

    Remove obsolete variable and function aliases

--- lisp/magit-obsolete.el
+++ lisp/magit-obsolete.el
@@ -29,43 +29,7 @@
 
 (require 'magit)
 
-;;; Obsolete since v2.1.0
-
-(define-obsolete-variable-alias 'magit-git-standard-options
-  'magit-git-global-arguments "Magit 2.1.0")
-
-;;; Obsolete since v2.2.0
-
-(define-obsolete-variable-alias 'magit-log-section-args
-  'magit-log-section-arguments "Magit 2.2.0")
-
-;;; Obsolete since v2.3.0
-
-(define-obsolete-function-alias 'global-magit-file-buffer-mode
-  'global-magit-file-mode "Magit 2.3.0")
-
 ;;; Obsolete since v2.4.0
-
-(define-obsolete-function-alias 'magit-get-tracked-ref
-  'magit-get-upstream-ref "Magit 2.4.0")
-
-(define-obsolete-function-alias 'magit-get-tracked-branch
-  'magit-get-upstream-branch "Magit 2.4.0")
-
-(define-obsolete-function-alias 'magit-get-tracked-remote
-  'magit-get-upstream-remote "Magit 2.4.0")
-
-(define-obsolete-function-alias 'magit-insert-head-header
-  'magit-insert-head-branch-header "Magit 2.4.0")
-
-(define-obsolete-function-alias 'magit-insert-pull-branch-header
-  'magit-insert-upstream-branch-header "Magit 2.4.0")
-
-(define-obsolete-function-alias 'magit-insert-unpulled-or-recent-commits
-  'magit-insert-unpulled-from-upstream-or-recent "Magit 2.4.0")
-
-(define-obsolete-function-alias 'magit-insert-upstream-header
-  'magit-insert-upstream-branch-header "Magit 2.4.0")
 
 (make-obsolete-variable 'magit-revert-buffers
                         'magit-auto-revert-mode
@@ -91,14 +55,6 @@ use `magit-pre-refresh-hook', `magit-post-refresh-hook',
   is first created, then `magit-status-mode-hook' instead.
 " "Magit 2.4.0")
 
-;;; Obsolete since v2.6.0
-
-(define-obsolete-function-alias 'magit-insert-unpulled-module-commits
-  'magit-insert-modules-unpulled-from-upstream "Magit 2.6.0")
-
-(define-obsolete-function-alias 'magit-insert-unpushed-module-commits
-  'magit-insert-modules-unpushed-to-upstream "Magit 2.6.0")
-
 ;;; Obsolete since v2.8.0
 
 (make-obsolete-variable 'magit-repository-directories-depth
@@ -106,36 +62,11 @@ use `magit-pre-refresh-hook', `magit-post-refresh-hook',
 
 ;;; Obsolete since v2.9.0
 
-(define-obsolete-variable-alias 'magit-duration-spec
-  'magit--age-spec "Magit 2.9.0")
-
 (make-obsolete-variable 'magit-diff-show-lines-boundary
                         'magit-diff-highlight-hunk-region-functions
                         "Magit 2.9.0")
 
-(define-obsolete-variable-alias 'magit-log-output-coding-system
-  'magit-git-output-coding-system "Magit 2.9.0")
-
-;;; Obsolete since v2.9.1
-
-(define-obsolete-function-alias 'magit-checkout-file
-  'magit-file-checkout "Magit 2.9.1")
-
-;;; Obsolete since v2.11.0
-
-(define-obsolete-function-alias 'magit-modified-files
-  'magit-unstaged-files "Magit 2.11.0")
-
-(define-obsolete-function-alias 'magit-insert-submodules
-  'magit-insert-modules-overview "Magit 2.11.0")
-
 ;;; Obsolete since v2.12.0
-
-(define-obsolete-function-alias 'magit-insert-unpulled-from-upstream-or-recent
-  'magit-insert-unpulled-from-upstream "Magit 2.12.0")
-
-(define-obsolete-function-alias 'magit-get-submodules
-  'magit-list-module-paths "Magit 2.12.0")
 
 (make-obsolete-variable 'magit-status-expand-stashes
                         'magit-section-initial-visibility-alist
