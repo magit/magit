5fa6a52edd45320d141eb46c650adffbfe853072
Jonas Bernoulli <jonas@bernoul.li>
2018-06-18 21:20:57 -0500

    cosmetics

--- lisp/magit-verify.el
+++ lisp/magit-verify.el
@@ -62,22 +62,21 @@ doing.
 
 If IGNORE-REVOCATION is non-nil, will accept signatures from
 revoked keys.  Enable if you know what you're doing."
-  (-non-nil
-   (mapcar (lambda (sig) (oref (magit-verify-signature
-                                sig
-                                ignore-key-expiration
-                                ignore-sig-expiration
-                                ignore-key-validity
-                                ignore-revocation)
-                               key-fingerprint))
-           (magit-read-commit-signatures id))))
+  (-keep (lambda (sig)
+           (oref (magit-verify-signature
+                  sig
+                  ignore-key-expiration
+                  ignore-sig-expiration
+                  ignore-key-validity
+                  ignore-revocation)
+                 key-fingerprint))
+         (magit-read-commit-signatures id)))
 
 (defun magit-verify-tag (name &optional
                               ignore-key-expiration
                               ignore-sig-expiration
                               ignore-key-validity
                               ignore-revocation)
-
   "Verify tag NAME.
 
 NAME must be a tag *name*, and cannot be a SHA-1 identifier.
@@ -89,15 +88,15 @@ be signaled.
 IGNORE-KEY-EXPIRATION, IGNORE-SIG-EXPIRATION, IGNORE-KEY-VALIDITY
 and IGNORE-REVOCATION have the same meaning as in
 `magit-verify-commit', which see."
-  (-non-nil
-   (mapcar (lambda (sig) (oref (magit-verify-signature
-                                sig
-                                ignore-key-expiration
-                                ignore-sig-expiration
-                                ignore-key-validity
-                                ignore-revocation)
-                               key-fingerprint))
-           (magit-read-tag-signatures name))))
+  (-keep (lambda (sig)
+           (oref (magit-verify-signature
+                  sig
+                  ignore-key-expiration
+                  ignore-sig-expiration
+                  ignore-key-validity
+                  ignore-revocation)
+                 key-fingerprint))
+         (magit-read-tag-signatures name)))
 
 ;;; Lower level interface
 
@@ -110,13 +109,12 @@ the same functionality.
 
 Return a possibly empty list of `magit-pgp-signature' objects, which see."
   (magit-verify--parse-output
-   (split-string
-    ;; `magit-git-lines' only returns stdout, we need stderr.
-    (shell-command-to-string (format
-                              "%s verify-commit --raw %s"
-                              (shell-quote-argument magit-git-executable)
-                              (shell-quote-argument id)))
-    "\n")))
+   ;; `magit-git-lines' only returns stdout, we need stderr.
+   (split-string (shell-command-to-string
+                  (format "%s verify-commit --raw %s"
+                          (shell-quote-argument magit-git-executable)
+                          (shell-quote-argument id)))
+                 "\n")))
 
 (defun magit-read-tag-signatures (name)
   "Return PGP signatures of tag NAME, regardless of their validity.
@@ -132,21 +130,19 @@ git verify-tag doesn't do.  If names don't match, an error will
 be signaled.
 
 Return a possibly empty list of `magit-pgp-signature' objects, which see."
-  (let ((realname (car (magit-git-lines "verify-tag" "--format" "%(tag)" name))))
-    (unless
-        (string= realname name)
-      (error
-       "The tag object referred to by `%s' is actually named `%s'.  Maybe you've given `magit-verify-tag' a hash instead of a name, or maybe something fishy is going on"
-       name realname)))
+  (let ((realname (magit-git-string "verify-tag" "--format" "%(tag)" name)))
+    (unless (string= realname name)
+      (error "The tag object referred to by `%s' is actually named `%s'.  \
+Maybe you've given `magit-verify-tag' a hash instead of a name, \
+or maybe something fishy is going on" name realname)))
 
   (magit-verify--parse-output
-   (split-string
-    ;; `magit-git-lines' only returns stdout, we need stderr.
-    (shell-command-to-string (format
-                              "%s verify-tag --raw %s"
-                              (shell-quote-argument magit-git-executable)
-                              (shell-quote-argument name)))
-    "\n")))
+   ;; `magit-git-lines' only returns stdout, we need stderr.
+   (split-string (shell-command-to-string
+                  (format "%s verify-tag --raw %s"
+                          (shell-quote-argument magit-git-executable)
+                          (shell-quote-argument name)))
+                 "\n")))
 
 (defun magit-verify-signature (sig &optional
                                    ignore-key-expiration
@@ -323,12 +319,10 @@ Short, minimal, version:
 
 (defun magit-verify--extract-uid (line sig)
   ;; Read name and comment from a "*SIG" LINE and `oset' them in SIG.
-
-  (unless
-      (string-match
-   ;; "^[A-Z]+SIG  \\(.*\\)\\(?: (<\\(.+\\))\\) <\\(.*\\)$>"
-       "^[A-Z]+SIG [[:xdigit:]]+ \\(.*?\\)\\(?: (\\(.*\\))\\)? <\\(.*\\)>$"
-       line)
+  (unless (string-match
+           ;; "^[A-Z]+SIG  \\(.*\\)\\(?: (<\\(.+\\))\\) <\\(.*\\)$>"
+           "^[A-Z]+SIG [[:xdigit:]]+ \\(.*?\\)\\(?: (\\(.*\\))\\)? <\\(.*\\)>$"
+           line)
     (error "Error parsing uid from %s" line))
   (oset sig key-name (match-string 1 line))
   (oset sig key-comment (match-string 2 line))
@@ -339,80 +333,71 @@ Short, minimal, version:
   ;;
   ;; If STR matches ^[[:digit:]]+$, it's treated as a unix timestmap,
   ;; otherwise as an RFC 8601 string.  If nothing works, raise an error."
-  (and
-   (not (string= str "0"))
-   (or
-    (and
-     (string-match "\\`[[:digit:]]+\\'" str)
-     (seconds-to-time (string-to-number str)))
-
-    ;; @FIXME If `parse-iso8601-time-string' fails, it will neither
-    ;; return an invalid value nor error, but instead delegate to
-    ;; the weird `parse-time-string' which will associate a value to
-    ;; every possible input.  This is not what we want, and we need
-    ;; a way to error if we can't parse the date.
-    (parse-iso8601-time-string str))))
+  (and (not (string= str "0"))
+       (or (and (string-match "\\`[[:digit:]]+\\'" str)
+                (seconds-to-time (string-to-number str)))
+           ;; @FIXME If `parse-iso8601-time-string' fails, it will neither
+           ;; return an invalid value nor error, but instead delegate to
+           ;; the weird `parse-time-string' which will associate a value to
+           ;; every possible input.  This is not what we want, and we need
+           ;; a way to error if we can't parse the date.
+           (parse-iso8601-time-string str))))
 
 (defun magit-verify--parse-output (lines)
   ;; Parse output from git verify-[tag|commit] --raw (which is
   ;; actually the output of gpg --verify --status-fd) and return a
   ;; possibly empty list of `magit-pgp-signature' object.
-  (let ((lines (-non-nil
-                (mapcar
-                 (lambda (line)
-                   (and
-                    (string-prefix-p "[GNUPG:] " line)
-                    (substring line 9)))
-                 lines)))
+  (let ((lines (cl-mapcan (lambda (line)
+                            (and (string-prefix-p "[GNUPG:] " line)
+                                 (list (substring line 9))))
+                          lines))
         (sigs nil)
         (sig nil))
-
     (message "%s" lines)
-
     (dolist (line lines nil)
       (let ((fields (split-string line)))
         (pcase (car fields)
         ;; Start reading a new signature
-          ("NEWSIG" (progn
-                      (setq sigs (cons (setq sig (magit-pgp-signature)) sigs))
-                      (oset sig key-uid (nth 1 fields))))
-          ("GOODSIG" (progn
-                       (oset sig sig-validity t)
-                       (oset sig sig-expired nil)
-                       (oset sig key-expired nil)
-                       (magit-verify--extract-uid line sig)))
-          ("EXPSIG" (progn
-                      (oset sig sig-validity t)
-                      (oset sig sig-expired t)
-                      (magit-verify--extract-uid line sig)))
-          ("EXPKEYSIG" (progn
-                         (oset sig sig-validity t)
-                         (oset sig key-expired t)
-                         (magit-verify--extract-uid line sig)))
-          ("REVKEYSIG" (progn
-                         (oset sig sig-validity t)
-                         (oset sig key-revoked t)
-                         (magit-verify--extract-uid line sig)))
-          ("ERRSIG" (progn
-                      (oset sig sig-validity nil)
-                      (oset sig error (pcase (number-to-string (nth 6 fields))
-                            (4 'unknown-algorithm)
-                            (9 'no-pubkey)
-                            (_ t)))))
-          ("BADSIG" (progn
-                      (oset sig sig-validity nil)
-                      (magit-verify--extract-uid line sig)))
-          ("VALIDSIG" (progn
-                        (oset sig key-fingerprint (nth 1 fields))
-                        (oset sig sig-creation-date (magit-verify--parse-date (nth 3 fields)))
-                        (oset sig sig-expiration-date (magit-verify--parse-date (nth 4 fields)))))
-          ("KEYEXPIRED" (oset sig key-expired t))
+          ("NEWSIG"
+           (setq sigs (cons (setq sig (magit-pgp-signature)) sigs))
+           (oset sig key-uid (nth 1 fields)))
+          ("GOODSIG"
+           (oset sig sig-validity t)
+           (oset sig sig-expired nil)
+           (oset sig key-expired nil)
+           (magit-verify--extract-uid line sig))
+          ("EXPSIG"
+           (oset sig sig-validity t)
+           (oset sig sig-expired t)
+           (magit-verify--extract-uid line sig))
+          ("EXPKEYSIG"
+           (oset sig sig-validity t)
+           (oset sig key-expired t)
+           (magit-verify--extract-uid line sig))
+          ("REVKEYSIG"
+           (oset sig sig-validity t)
+           (oset sig key-revoked t)
+           (magit-verify--extract-uid line sig))
+          ("ERRSIG"
+           (oset sig sig-validity nil)
+           (oset sig error (pcase (number-to-string (nth 6 fields))
+                             (4 'unknown-algorithm)
+                             (9 'no-pubkey)
+                             (_ t))))
+          ("BADSIG"
+           (oset sig sig-validity nil)
+           (magit-verify--extract-uid line sig))
+          ("VALIDSIG"
+           (oset sig key-fingerprint (nth 1 fields))
+           (oset sig sig-creation-date (magit-verify--parse-date (nth 3 fields)))
+           (oset sig sig-expiration-date (magit-verify--parse-date (nth 4 fields))))
+          ("KEYEXPIRED"      (oset sig key-expired t))
           ("TRUST_UNDEFINED" (oset sig key-validity 'undefined))
-          ("TRUST_NEVER" (oset sig key-validity 'never))
-          ("TRUST_MARGINAL" (oset sig key-validity 'marginal))
-          ("TRUST_FULLY" (oset sig key-validity 'fully))
-          ("TRUST_ULTIMATE" (oset sig key-validity 'ultimate))
-          ("NO_PUBKEY" (oset sig error 'no-pubkey))
+          ("TRUST_NEVER"     (oset sig key-validity 'never))
+          ("TRUST_MARGINAL"  (oset sig key-validity 'marginal))
+          ("TRUST_FULLY"     (oset sig key-validity 'fully))
+          ("TRUST_ULTIMATE"  (oset sig key-validity 'ultimate))
+          ("NO_PUBKEY"       (oset sig error 'no-pubkey))
 
           ;; Arguments we know we can ignore
           ("SIG_ID")
@@ -421,11 +406,13 @@ Short, minimal, version:
 
           ;; Error states We may have received more specific
           ;; information earlier, we don't want to overwrite them.
-          ("NODATA" (unless (member (string-to-number (nth 1 fields)) '(1 2))
-                      (error "PGP returned abnormal state NODATA %s" (nth 1 fields))))
-           ("FAILURE" (unless (or (not sig)
-                                  (oref sig error))
-                        (oset sig error t)))
+          ("NODATA"
+           (unless (member (string-to-number (nth 1 fields)) '(1 2))
+             (error "PGP returned abnormal state NODATA %s" (nth 1 fields))))
+          ("FAILURE"
+           (unless (or (not sig)
+                       (oref sig error))
+             (oset sig error t)))
 
           ;; The doc in doc/DETAILS states that: "an application should
           ;; always be willing to ignore unknown keywords that may be
