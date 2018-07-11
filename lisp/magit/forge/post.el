0c2d080fe90da014056c0c1ae94eae9606d24491
Jonas Bernoulli <jonas@bernoul.li>
2018-07-11 17:51:00 -0500

    [DUMP] posts

--- lisp/magit/forge/post.el
+++ lisp/magit/forge/post.el
@@ -31,6 +31,60 @@
 
 (defclass magit-forge-post (magit-forge-object) () :abstract t)
 
+;;; Sections
+
+(defun magit-post-at-point ()
+  (magit-section-when (issue pullreq post)))
+
+(defvar magit-post-section-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-e") 'magit-forge-edit-post)
+    map))
+
+;;; Mode
+
+(defvar magit-forge-post-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-c") 'magit-forge-post-submit)
+    (define-key map (kbd "C-c C-k") 'magit-forge-post-cancel)
+    map))
+
+(define-minor-mode magit-forge-post-mode
+  "Edit a forge issue, pull-request or post.")
+
+(defvar-local magit-forge-buffer-post nil)
+
+;;; Commands
+
+(defun magit-forge-edit-post (post)
+  (interactive (list (magit-post-at-point)))
+  (let ((file (magit-git-dir (convert-standard-filename
+                              (concat "magit/posts/" (oref post id))))))
+    (when (file-exists-p file)
+      (delete-file file))
+    (if-let ((buf (find-buffer-visiting file)))
+        (pop-to-buffer buf)
+      (make-directory (file-name-directory file) t)
+      (find-file-other-window file)
+      (erase-buffer) ; TODO offer to use draft
+      (save-excursion
+        (insert (oref post body)))
+      (if (fboundp 'gfm-mode)
+          (gfm-mode)
+        (text-mode))
+      (visual-line-mode))
+    (setq magit-forge-buffer-post post)
+    (magit-forge-post-mode)))
+
+(defun magit-forge-post-submit ()
+  (interactive)
+  (cl-typecase magit-forge-buffer-post
+    (magit-forge-post (message "post"))))
+
+(defun magit-forge-post-cancel ()
+  (interactive)
+  (kill-buffer))
+
 ;;; _
 (provide 'magit/forge/post)
 ;;; magit/forge/post.el ends here
