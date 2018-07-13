826746e067a98693f1bf63c5f203a6a2e68c4155
Jonas Bernoulli <jonas@bernoul.li>
2018-07-13 16:31:24 -0500

    .dir-locals.el: Set git-commit-major-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,6 @@
   (bug-reference-bug-regexp . "#\\(?2:[0-9]+\\)")
   (bug-reference-url-format . "https://github.com/magit/magit/issues/%s"))
  (makefile-gmake-mode
-  (outline-regexp . "##")))
+  (outline-regexp . "##"))
+ (git-commit-mode
+  (git-commit-major-mode . git-commit-elisp-text-mode)))
