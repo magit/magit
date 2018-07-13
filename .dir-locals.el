ad6ae0813f5daea5bfffd90b9dbd3aa95fc51a14
Jonas Bernoulli <jonas@bernoul.li>
2018-07-13 16:32:23 -0500

    .dir-locals.el: Enable bug-reference-mode in emacs-lisp-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,7 @@
   (bug-reference-bug-regexp . "#\\(?2:[0-9]+\\)")
   (bug-reference-url-format . "https://github.com/magit/magit/issues/%s"))
  (emacs-lisp-mode
+  (eval . (bug-reference-mode 1))
   (indent-tabs-mode . nil))
  (makefile-gmake-mode
   (outline-regexp . "##"))
