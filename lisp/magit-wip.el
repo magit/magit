fc201d7efbb56759c1483489c023b271152dc07c
Kyle Meyer <kyle@kyleam.com>
2018-08-04 12:18:00 -0400

    magit--wip-ref: Replace and-let* with when-let
    and-let* isn't available until Emacs 26.1.

--- lisp/magit-wip.el
+++ lisp/magit-wip.el
@@ -301,7 +301,7 @@ commit message."
 (defun magit--wip-ref (namespace &optional ref)
   (concat magit-wip-namespace namespace
           (or (and ref (string-prefix-p "refs/" ref) ref)
-              (and-let* ((branch (or ref (magit-get-current-branch))))
+              (when-let ((branch (or ref (magit-get-current-branch))))
                 (concat "refs/heads/" branch))
               "HEAD")))
 
