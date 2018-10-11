728052be437fbc606381244125e786c2f65e576b
Jonas Bernoulli <jonas@bernoul.li>
2018-10-11 20:33:00 +0200

    Rename magit-merge to magit-merge-plain
    See #3619.

--- lisp/magit-obsolete.el
+++ lisp/magit-obsolete.el
@@ -41,6 +41,7 @@
 (define-obsolete-function-alias 'magit-blame 'magit-blame-addition "Magit 2.90.0")
 (define-obsolete-function-alias 'magit-revert 'magit-revert-and-commit "Magit 2.90.0")
 (define-obsolete-function-alias 'magit-reset 'magit-reset-quickly "Magit 2.90.0")
+(define-obsolete-function-alias 'magit-merge 'magit-merge-plain "Magit 2.90.0")
 
 ;;; _
 (provide 'magit-obsolete)
