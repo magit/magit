e9bd3375854ec9406ff40bfb453cb28e9e5676ea
Jonas Bernoulli <jonas@bernoul.li>
2018-11-16 14:34:59 +0100

    Correct minimal git-commit version

--- default.mk
+++ default.mk
@@ -101,7 +101,7 @@ VERSION ?= $(shell test -e $(TOP).git && git describe --tags --abbrev=0 | cut -c
 ASYNC_VERSION       = 1.9.3
 DASH_VERSION        = 2.14.1
 GHUB_VERSION        = 3.0.0
-GIT_COMMIT_VERSION  = 2.13.0
+GIT_COMMIT_VERSION  = 2.90.0
 MAGIT_POPUP_VERSION = 2.12.4
 WITH_EDITOR_VERSION = 2.8.0
 
