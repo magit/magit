59f7329cfe5705db391a8b37983459e632fa53e3
Jonas Bernoulli <jonas@bernoul.li>
2018-11-26 10:14:00 +0100

    magit--overlay-at: New utility function

--- lisp/magit-utils.el
+++ lisp/magit-utils.el
@@ -887,6 +887,16 @@ that it will align with the text area."
   (interactive)
   (kill-buffer (current-buffer)))
 
+(cl-defun magit--overlay-at (pos prop &optional (val nil sval) testfn)
+  (cl-find-if (lambda (o)
+                (let ((p (overlay-properties o)))
+                  (and (plist-member p prop)
+                       (or (not sval)
+                           (funcall (or testfn #'eql)
+                                    (plist-get p prop)
+                                    val)))))
+              (overlays-at pos t)))
+
 ;;; Kludges for Emacs Bugs
 
 (defun magit-file-accessible-directory-p (filename)
