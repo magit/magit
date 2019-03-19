3a213ef9a78ae3bea24a8bacc2f003f5ce975875
Keshav Kini <keshav.kini@gmail.com>
2019-03-18 18:53:17 -0700

    Fix typo in `magit-cherry-pick:--mainline`
    The transient library doesn't have a function called
    transient-read-natural-number-N+.  This seems to be a typo for
    transient-read-number-N+, which does exist, and makes `A - m` work as
    expected rather than suddenly closing the transient.

--- lisp/magit-sequence.el
+++ lisp/magit-sequence.el
@@ -164,7 +164,7 @@ This discards all changes made since the sequence started."
   :class 'transient-option
   :shortarg "-m"
   :argument "--mainline="
-  :reader 'transient-read-natural-number-N+)
+  :reader 'transient-read-number-N+)
 
 (defun magit-cherry-pick-read-args (prompt)
   (list (or (nreverse (magit-region-values 'commit))
