5923b2b144f890a8a3aeb8cbc62367ef1fd5122d
Jonas Bernoulli <jonas@bernoul.li>
2019-04-16 08:38:00 +0200

    magit-dired-log: Use magit-log-setup-buffer

--- lisp/magit-extras.el
+++ lisp/magit-extras.el
@@ -154,12 +154,11 @@ is no file at point, then instead visit `default-directory'."
                    (not (member "--follow" args))
                    (not (cdr files)))
           (push "--follow" args))
-        (magit-mode-setup-internal
-         #'magit-log-mode
-         (list (list (or (magit-get-current-branch) "HEAD"))
-               args
-               (let ((default-directory topdir))
-                 (mapcar #'file-relative-name files)))
+        (magit-log-setup-buffer
+         (list (or (magit-get-current-branch) "HEAD"))
+         args
+         (let ((default-directory topdir))
+           (mapcar #'file-relative-name files))
          magit-log-buffer-file-locked))
     (magit--not-inside-repository-error)))
 
