86274038d658a1a99b6795e130cd51cbd848e18a
Jonas Bernoulli <jonas@bernoul.li>
2019-04-16 08:35:00 +0200

    magit-log-setup-buffer: Add LOCKED argument

--- lisp/magit-log.el
+++ lisp/magit-log.el
@@ -937,9 +937,9 @@ Type \\[magit-reset] to reset `HEAD' to the commit at point.
   (setq imenu-extract-index-name-function
         'magit-imenu--log-extract-index-name-function))
 
-(defun magit-log-setup-buffer (revs args files)
+(defun magit-log-setup-buffer (revs args files &optional locked)
   (require 'magit)
-  (magit-mode-setup #'magit-log-mode revs args files)
+  (magit-mode-setup-internal #'magit-log-mode (list revs args files) locked)
   (magit-log-goto-same-commit))
 
 (defun magit-log-refresh-buffer (revs args files)
