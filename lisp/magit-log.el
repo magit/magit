75018ac991b8ab4d07193a8b00210855a10b85e4
Jonas Bernoulli <jonas@bernoul.li>
2019-11-28 01:39:02 +0100

    magit-log-propertize-keywords: Rename internal variable
    "From beginning to start" is just too weird.

--- lisp/magit-log.el
+++ lisp/magit-log.el
@@ -1261,15 +1261,15 @@ Do not add this to a hook variable."
   t)
 
 (defun magit-log-propertize-keywords (_rev msg)
-  (let ((start 0))
-    (when (string-match "^\\(?:squash\\|fixup\\)! " msg start)
-      (setq start (match-end 0))
-      (magit--put-face (match-beginning 0) (1- start)
+  (let ((boundary 0))
+    (when (string-match "^\\(?:squash\\|fixup\\)! " msg boundary)
+      (setq boundary (match-end 0))
+      (magit--put-face (match-beginning 0) (1- boundary)
                        'magit-keyword-squash msg))
     (when magit-log-highlight-keywords
-      (while (string-match "\\[[^[]*?]" msg start)
-        (setq start (match-end 0))
-        (magit--put-face (match-beginning 0) start
+      (while (string-match "\\[[^[]*?]" msg boundary)
+        (setq boundary (match-end 0))
+        (magit--put-face (match-beginning 0) boundary
                          'magit-keyword msg))))
   msg)
 
