magit/magit

commit 404bd1148a593e4b21b10596b9af68f7c4a27e26

magit--wip{,-index,-wtree}-ref: New functions

lisp/magit-wip.el
@@ -192,13 +192,13 @@ commit message."
     (magit-wip-commit-worktree it files msg)))
 
 (defun magit-wip-commit-index (ref files msg)
-  (let* ((wipref (concat magit-wip-namespace "index/" ref))
+  (let* ((wipref (magit--wip-index-ref ref))
          (parent (magit-wip-get-parent ref wipref))
          (tree   (magit-git-string "write-tree")))
     (magit-wip-update-wipref ref wipref tree parent files msg "index")))
 
 (defun magit-wip-commit-worktree (ref files msg)
-  (let* ((wipref (concat magit-wip-namespace "wtree/" ref))
+  (let* ((wipref (magit--wip-wtree-ref ref))
          (parent (magit-wip-get-parent ref wipref))
          (tree (magit-with-temp-index parent "--reset"
                  (if files
@@ -259,6 +259,19 @@ commit message."
       wipref
     ref))
 
+(defun magit--wip-index-ref (&optional ref)
+  (magit--wip-ref "index/" ref))
+
+(defun magit--wip-wtree-ref (&optional ref)
+  (magit--wip-ref "wtree/" ref))
+
+(defun magit--wip-ref (namespace &optional ref)
+  (concat magit-wip-namespace namespace
+          (or (and ref (string-prefix-p "refs/" ref) ref)
+              (and-let* ((branch (or ref (magit-get-current-branch))))
+                (concat "refs/heads/" branch))
+              "HEAD")))
+
 ;;; Log
 
 (defun magit-wip-log-current (branch args files count)
@@ -287,15 +300,13 @@ many \"branches\" of each wip ref are shown."
                      "HEAD")))
           (magit-log-arguments)
           (list (prefix-numeric-value current-prefix-arg))))
-  (unless (equal branch "HEAD")
-    (setq branch (concat "refs/heads/" branch)))
   (magit-log (nconc (list branch)
                     (magit-wip-log-get-tips
-                     (concat magit-wip-namespace "wtree/" branch)
+                     (magit--wip-wtree-ref branch)
                      (abs count))
                     (and (>= count 0)
                          (magit-wip-log-get-tips
-                          (concat magit-wip-namespace "index/" branch)
+                          (magit--wip-index-ref branch)
                           (abs count))))
              args files))
 

commit 798e47a1f07ac40c5a8c82372814c52be0478a76

magit--wip-ref: Fix regression in handling of HEAD
404bd1148 (magit--wip{,-index,-wtree}-ref: New functions, 2018-08-02)
introduced magit--wip-ref to handle the construction of wip refs.  For
a detached head, the wip ref should be ".../{wtree,index}/HEAD".
magit--wip-ref returns the correct wip ref for a detached HEAD when
REF is nil.  However, it incorrectly constructs the ref as
"{wtree,index}/refs/heads/HEAD" when "HEAD" is passed as REF.  As a
result, commits for a detached HEAD are made on
"{wtree,index}/refs/heads/HEAD".

Add a condition to magit--wip-wtree-ref to avoid treating a REF of
"HEAD" as a regular branch.

Documentation/RelNotes/3.0.0.org
@@ -444,6 +444,10 @@
 - ~magit-stash-branch-here~ applied the stash without checking out the
   new branch.  d3cee7f8c
 
+- A regression in v2.90.0 led to ~magit-wip-mode~ committing to "<wip
+  prefix>/refs/heads/HEAD" rather than "<wip prefix>/HEAD" when on a
+  detached head.
+
 - Various bug fixes to
   ~magit-branch-delete~ (3e73ff19d),
   ~magit--{upstream,pushbranch}-suffix-predicate~ (0ce7fbbc2),

lisp/magit-wip.el
@@ -353,7 +353,8 @@ commit message."
 (defun magit--wip-ref (namespace &optional ref)
   (concat magit-wip-namespace namespace
           (or (and ref (string-prefix-p "refs/" ref) ref)
-              (when-let ((branch (or ref (magit-get-current-branch))))
+              (when-let ((branch (and (not (equal ref "HEAD"))
+                                      (or ref (magit-get-current-branch)))))
                 (concat "refs/heads/" branch))
               "HEAD")))
 