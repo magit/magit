d05545ec2fd7edf915eaf1b9c15c785bb08975cc
Jonas Bernoulli <jonas@bernoul.li>
2020-05-14 15:30:20 +0200

    magit-list-publishing-branches: If commit is nil then use HEAD
    That was the intention all along; `magit-commit-amend-assert' calls
    this function expecting this.  "git merge-base --is-ancestor <commit>
    <commit>" needs two commits, neither defaults to "HEAD".

--- lisp/magit-git.el
+++ lisp/magit-git.el
@@ -1603,7 +1603,7 @@ SORTBY is a key or list of keys to pass to the `--sort' flag of
   (magit-list-related-branches "--contains" commit args))
 
 (defun magit-list-publishing-branches (&optional commit)
-  (--filter (magit-rev-ancestor-p commit it)
+  (--filter (magit-rev-ancestor-p (or commit "HEAD") it)
             magit-published-branches))
 
 (defun magit-list-merged-branches (&optional commit &rest args)
