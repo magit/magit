85e88c17cde9bc16ec137de3cff4ceed0f0fc83d
Jonas Bernoulli <jonas@bernoul.li>
2020-07-01 23:12:28 +0200

    git-commit-major-mode: Offer more choices including arbitrary mode

--- lisp/git-commit.el
+++ lisp/git-commit.el
@@ -175,6 +175,9 @@ The major mode configured here is turned on by the minor mode
 `git-commit-mode'."
   :group 'git-commit
   :type '(choice (function-item text-mode)
+                 (function-item markdown-mode)
+                 (function-item org-mode)
+                 (function :tag "Another mode")
                  (const :tag "No major mode")))
 
 (defcustom git-commit-setup-hook
