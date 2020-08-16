321214c3a2dd10fdf672ba96bd00703a51094bbe
Jonas Bernoulli <jonas@bernoul.li>
2020-08-16 11:55:36 +0200

    magit-insert-section: Avoid undefined if-let
    Don't use `if-let' because we only require `subr-x' at compile
    time, so when this macro is expanded in some third-party code,
    then `subr-x' might not have been loaded.

--- lisp/magit-section.el
+++ lisp/magit-section.el
@@ -1052,21 +1052,24 @@ form `(eval FORM)' in which case FORM is evaluated at runtime.
                          :start (point-marker)
                          :parent magit-insert-section--parent)))
        (oset ,s hidden
-             (if-let ((value (run-hook-with-args-until-success
-                              'magit-section-set-visibility-hook ,s)))
-                 (eq value 'hide)
-               (if-let ((incarnation (and magit-insert-section--oldroot
-                                          (magit-get-section
-                                           (magit-section-ident ,s)
-                                           magit-insert-section--oldroot))))
-                   (oref incarnation hidden)
-                 (if-let ((value (magit-section-match-assoc
-                                  ,s magit-section-initial-visibility-alist)))
-                     (progn
-                       (when (functionp value)
-                         (setq value (funcall value ,s)))
-                       (eq value 'hide))
-                   ,(nth 2 (car args))))))
+             (let ((value (run-hook-with-args-until-success
+                           'magit-section-set-visibility-hook ,s)))
+               (if value
+                   (eq value 'hide)
+                 (let ((incarnation (and magit-insert-section--oldroot
+                                         (magit-get-section
+                                          (magit-section-ident ,s)
+                                          magit-insert-section--oldroot))))
+                   (if incarnation
+                       (oref incarnation hidden)
+                     (let ((value (magit-section-match-assoc
+                                   ,s magit-section-initial-visibility-alist)))
+                       (if value
+                           (progn
+                             (when (functionp value)
+                               (setq value (funcall value ,s)))
+                             (eq value 'hide))
+                         ,(nth 2 (car args)))))))))
        (let ((magit-insert-section--current ,s)
              (magit-insert-section--parent  ,s)
              (magit-insert-section--oldroot
