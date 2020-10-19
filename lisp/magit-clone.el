64341b02d24144b519a3f06362f0a7e839ef7189
Basil L. Contovounesios <contovob@tcd.ie>
2020-10-19 12:13:55 +0100

    magit-clone-internal: Fix user-error arguments

--- lisp/magit-clone.el
+++ lisp/magit-clone.el
@@ -208,8 +208,8 @@ Then show the status buffer for the new repository."
                            (setq directory (file-name-as-directory
                                             (expand-file-name name directory)))
                            (not (file-exists-p directory)))
-                (user-error "%s already exits"))))
-        (user-error "%s already exists and is not a directory")))
+                (user-error "%s already exists" directory))))
+        (user-error "%s already exists and is not a directory" directory)))
     (magit-run-git-async "clone" args "--" repository
                          (magit-convert-filename-for-git directory))
     ;; Don't refresh the buffer we're calling from.
