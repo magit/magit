af6da4f9ec0562598ec9f37401c21aa0c888ea5d
Naoya Yamashita <conao3@gmail.com>
2021-03-08 10:04:45 +0900

    remove git-commit-major-mode entry from .dir-locals.el
    This variable is not *safe* varibale, warning window is noisy for
    new contributors.  Answering y to warnings is a bad habit to get
    beginners into.  Think about what it means for .dir-locals to
    show a warning for setting an unsafe variable, and if we need
    this, we should do it in dir-locals-2.el.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,4 @@
 ((emacs-lisp-mode
   (indent-tabs-mode . nil))
  (makefile-gmake-mode
-  (outline-regexp . "##"))
- (git-commit-mode
-  (git-commit-major-mode . git-commit-elisp-text-mode)))
+  (outline-regexp . "##")))
