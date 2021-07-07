098844104a9b9776a9ff0f78f7a99f0be664f284
Jonas Bernoulli <jonas@bernoul.li>
2021-07-07 20:59:49 +0200

    magit-mode: Set revert-buffer-function locally
    Closes #4432.

--- lisp/magit-mode.el
+++ lisp/magit-mode.el
@@ -513,6 +513,7 @@ Magit is documented in info node `(magit)'."
   (hack-dir-local-variables-non-file-buffer)
   (face-remap-add-relative 'header-line 'magit-header-line)
   (setq mode-line-process (magit-repository-local-get 'mode-line-process))
+  (setq-local revert-buffer-function 'magit-refresh-buffer)
   (setq-local bookmark-make-record-function 'magit--make-bookmark)
   (setq-local isearch-filter-predicate 'magit-section--open-temporarily))
 
@@ -1056,7 +1057,7 @@ Run hooks `magit-pre-refresh-hook' and `magit-post-refresh-hook'."
 
 (defvar-local magit-refresh-start-time nil)
 
-(defun magit-refresh-buffer ()
+(defun magit-refresh-buffer (&rest _ignore)
   "Refresh the current Magit buffer."
   (setq magit-refresh-start-time (current-time))
   (let ((refresh (intern (format "%s-refresh-buffer"
