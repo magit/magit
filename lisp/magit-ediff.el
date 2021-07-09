9b7b25f1e460f79de894f583727635134b7513a2
Jonas Bernoulli <jonas@bernoul.li>
2021-07-09 18:04:00 +0200

    magit-ediff-use-indirect-buffers: New option
    Closes #4395.

--- lisp/magit-ediff.el
+++ lisp/magit-ediff.el
@@ -101,6 +101,12 @@ tree at the time of stashing."
   :group 'magit-ediff
   :type 'boolean)
 
+(defcustom magit-ediff-use-indirect-buffers nil
+  "Whether to use indirect buffers."
+  :package-version '(magit . "3.1.0")
+  :group 'magit-ediff
+  :type 'boolean)
+
 ;;; Commands
 
 (defvar magit-ediff-previous-winconf nil)
@@ -165,7 +171,15 @@ conflicts, including those already resolved by Git, use
     (pcase-dolist (`(,g ,m) spec)
       (let ((b (intern (format "buf%c" (cl-incf char)))))
         (push `(,b ,g) get)
-        (push `(or ,b ,g) make)
+        (push `(if ,b
+                   (if magit-ediff-use-indirect-buffers
+                       (prog1
+                           (make-indirect-buffer
+                            ,b (generate-new-buffer-name (buffer-name ,b)) t)
+                         (setq ,b nil))
+                     ,b)
+                 ,m)
+              make)
         (push `(unless ,b (ediff-kill-buffer-carefully ediff-buffer-A)) kill)))
     (setq get  (nreverse get))
     (setq make (nreverse make))
