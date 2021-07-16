cdfa805a0190761978ddc3e96b981e9eff8921ba
Brian Leung <leungbk@mailfence.com>
2021-07-15 18:50:23 -0700

    magit-extras: Check that `project-switch-commands' is defined

--- lisp/magit-extras.el
+++ lisp/magit-extras.el
@@ -157,6 +157,7 @@ to nil before loading Magit to prevent \"m\" from being bound.")
   ;; `project-switch-commands', though project.el is available in Emacs 25.
   (when (and magit-bind-magit-project-status
              (boundp 'project-prefix-map)
+             (boundp 'project-switch-commands)
              ;; Only modify if it hasn't already been modified.
              (equal project-switch-commands
                     (eval (car (get 'project-switch-commands 'standard-value))
