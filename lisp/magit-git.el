c7fd760d997ebacc82ba2f6dffaace945eb0693d
Kyle Meyer <kyle@kyleam.com>
2022-01-17 21:08:31 -0500

    magit-revision-directories: New function
    The ability to list subdirectories will be useful in the upcoming
    magit-sparse-checkout.el.

--- lisp/magit-git.el
+++ lisp/magit-git.el
@@ -961,6 +961,12 @@ tracked file."
   (magit-with-toplevel
     (magit-git-items "ls-tree" "-z" "-r" "--name-only" rev)))
 
+(defun magit-revision-directories (rev)
+  "List directories that contain a tracked file in revision REV."
+  (magit-with-toplevel
+    (mapcar #'file-name-as-directory
+            (magit-git-items "ls-tree" "-z" "-r" "-d" "--name-only" rev))))
+
 (defun magit-changed-files (rev-or-range &optional other-rev)
   "Return list of files the have changed between two revisions.
 If OTHER-REV is non-nil, REV-OR-RANGE should be a revision, not a
