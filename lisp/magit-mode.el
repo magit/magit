b280fd76250f8caaf1bc7c91800d9a577e017812
Jonas Bernoulli <jonas@bernoul.li>
2022-04-22 18:15:08 +0200

    Use compat-assoc-delete-all

--- lisp/magit-mode.el
+++ lisp/magit-mode.el
@@ -1413,9 +1413,7 @@ Unless specified, REPOSITORY is the current buffer's repository."
   (when-let ((cache (assoc (or repository
                                (magit-repository-local-repository))
                            magit-repository-local-cache)))
-    ;; There is no `assoc-delete-all'.
-    (setf (cdr cache)
-          (cl-delete key (cdr cache) :key #'car :test #'equal))))
+    (setf cache (compat-assoc-delete-all key cache))))
 
 (defmacro magit--with-repository-local-cache (key &rest body)
   (declare (indent 1) (debug (form body)))
