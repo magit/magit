ef9c7e0981e68f639b45f8c21301ac2136c61769
Johan von Tangen Sivertsen <johanvts@gmail.com>
2022-07-01 11:48:06 +0200

    lstrip
    use lstrip=2 to get short names without the slowness of 'short' (and without the ambiguity checking).

--- lisp/magit-git.el
+++ lisp/magit-git.el
@@ -1903,7 +1903,7 @@ SORTBY is a key or list of keys to pass to the `--sort' flag of
             (magit-git-lines "show-ref"))))
 
 (defun magit-list-refnames (&optional namespaces include-special)
-  (nconc (magit-list-refs namespaces "%(refname)")
+  (nconc (magit-list-refs namespaces "%(refname:lstrip=2)")
          (and include-special
               (magit-list-special-refnames))))
 
