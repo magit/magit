magit/magit

commit 5ddf251823227cbe64ddf406201d5fb1a7c3fc02

git-rebase-mode-show-keybindings: Propertize all keys
Do not propertize their padding.

lisp/git-rebase.el
@@ -783,9 +783,10 @@ By default, this is the same except for the \"pick\" command."
                   nil t))
         (goto-char (line-beginning-position))
         (pcase-dolist (`(,cmd . ,desc) git-rebase-command-descriptions)
-          (insert (format "%s %-8s %s\n"
+          (insert (format "%s %s %s\n"
                           comment-start
-                          (substitute-command-keys (format "\\[%s]" cmd))
+                          (string-pad
+                           (substitute-command-keys (format "\\[%s]" cmd)) 8)
                           desc)))
         (while (re-search-forward (concat git-rebase-comment-re
                                           "\\(  ?\\)\\([^\n,],\\) "
@@ -797,13 +798,16 @@ By default, this is the same except for the \"pick\" command."
                                (1+ (line-end-position)))
               (replace-match " " t t nil 1)
               (replace-match
-               (format
-                "%-8s"
-                (mapconcat #'key-description
+               (string-pad
+                (mapconcat (lambda (key)
+                             (save-match-data
+                               (substitute-command-keys
+                                (format "\\`%s'" (key-description key)))))
                            (cl-remove-if (lambda (key) (eq (elt key 0) 'menu-bar))
                                          (reverse (where-is-internal
                                                    cmd git-rebase-mode-map)))
-                           ", "))
+                           ", ")
+                8)
                t t nil 2))))))))
 
 (add-hook 'git-rebase-mode-hook #'git-rebase-mode-show-keybindings t)

commit c8df0fe620ffa4bff712aaded68db25f600c965b

git-rebase-mode-show-keybindings: fix key display for mapped commands
This is a regression from 5ddf251 which addressed a font lock change in
Emacs 28.1. That commit routed `git-rebase-command-descriptions' through
`substitute-command-keys' to propertize them but commands (and therefore
the keys) in `git-rebase-*' were then rendered, for example, as \'c'.

This patch routes all the mapped commands through
`substitute-command-keys' so that users have a consistent rendering:
either all of the keys are not font locked (Emacs 27) or they are font
locked (Emacs 28 and beyond).

lisp/git-rebase.el
@@ -806,14 +806,8 @@ By default, this is the same except for the \"pick\" command."
                 (replace-match " " t t nil 2)
                 (replace-match
                  (string-pad
-                  (mapconcat (lambda (key)
-                               (save-match-data
-                                 (substitute-command-keys
-                                  (format "\\`%s'" (key-description key)))))
-                             (cl-remove-if (lambda (key) (eq (elt key 0) 'menu-bar))
-                                           (reverse (where-is-internal
-                                                     cmd git-rebase-mode-map)))
-                             ", ")
+                  (save-match-data
+                    (substitute-command-keys (format "\\[%s]" cmd)))
                   8)
                  t t nil 3)))))))))
 