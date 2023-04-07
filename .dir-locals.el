9b2bc1cb0116881433c6b748141a878460662b33
Jonas Bernoulli <jonas@bernoul.li>
2023-04-07 12:30:53 +0200

    make: Enable outline-minor-mode and tweak outline-regexp

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,7 @@
 ((emacs-lisp-mode
   (indent-tabs-mode . nil))
  (makefile-gmake-mode
-  (outline-regexp . "##"))
+  (outline-regexp . "#\\(#+\\)")
+  (mode . outline-minor))
  (git-commit-mode
   (git-commit-major-mode . git-commit-elisp-text-mode)))
