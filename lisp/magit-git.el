654b8ceff31f3a0401d20c64fdb69b042bb1c81e
Jonas Bernoulli <jonas@bernoul.li>
2023-04-28 14:44:24 +0200

    magit-list-worktrees: Use -z only if Git supports it
    Closes #4930.

--- lisp/magit-git.el
+++ lisp/magit-git.el
@@ -2130,7 +2130,9 @@ specified using `core.worktree'."
                          ;; KLUDGE At least in Git v2.8.3 this argument
                          ;; would trigger a segfault.
                          (remove "--no-pager" magit-git-global-arguments)))
-                    (magit-git-items "worktree" "list" "--porcelain" "-z")))
+                    (if (magit-git-version>= "2.36")
+                        (magit-git-items "worktree" "list" "--porcelain" "-z")
+                      (magit-git-lines "worktree" "list" "--porcelain"))))
       (cond ((string-prefix-p "worktree" line)
              (let ((path (substring line 9)))
                (when remote
