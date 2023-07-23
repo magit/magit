bdade489ead0ffd1de0709042d5166837efea4e8
Jonas Bernoulli <jonas@bernoul.li>
2023-07-23 18:01:02 +0200

    magit-stash-{apply,pop}: Fix case when there are staged changes
    It is not possible to preserve a stash index when applying it and
    there are staged changes.  Previously we just always tried to preserve
    it anyway, using the `--index' argument, and only failed we fell back
    to applying without that argument.
    
    That used to work, but due to a change/regression in Git that stopped
    working quite some time ago.  The futile attempt to use `--index'
    causes the staged changes to be unstaged and when there are unstaged
    changes (to files also touched by the stash), then it is not possible
    to apply a stash at all.
    
    (Users still have to be aware that they might have to stage before
    being able to apply a stash, despite the presence of uncommitted
    changes.)

--- lisp/magit-stash.el
+++ lisp/magit-stash.el
@@ -237,23 +237,23 @@ specifying a list of files to be stashed."
 ;;;###autoload
 (defun magit-stash-apply (stash)
   "Apply a stash to the working tree.
-Try to preserve the stash index.  If that fails because there
-are staged changes, apply without preserving the stash index."
+If nothing is staged, then try to reinstate the stashed index.
+Doing so is not possible if there are staged changes."
   (interactive (list (magit-read-stash "Apply stash")))
-  (if (= (magit-call-git "stash" "apply" "--index" stash) 0)
-      (magit-refresh)
-    (magit-run-git "stash" "apply" stash)))
+  (magit-run-git "stash" "apply" stash
+                 (and (not (magit-anything-staged-p))
+                      "--index")))
 
 ;;;###autoload
 (defun magit-stash-pop (stash)
   "Apply a stash to the working tree and remove it from stash list.
-Try to preserve the stash index.  If that fails because there
-are staged changes, apply without preserving the stash index
-and forgo removing the stash."
+If nothing is staged, then try to reinstate the stashed index.
+Doing so is not possible if there are staged changes.  Do not
+remove the stash, if it cannot be applied."
   (interactive (list (magit-read-stash "Pop stash")))
-  (if (= (magit-call-git "stash" "apply" "--index" stash) 0)
-      (magit-stash-drop stash)
-    (magit-run-git "stash" "apply" stash)))
+  (magit-run-git "stash" "apply" stash
+                 (and (not (magit-anything-staged-p))
+                      "--index")))
 
 ;;;###autoload
 (defun magit-stash-drop (stash)
