magit/magit

commit f6f4b96b641a5af75e90b5431a32369ab0f664a7

magit-stash-{apply,pop}: Ignore unrelated files to preserve index
The stash index cannot preserved when the stashed files are
also modified in the working tree, but other modified files
do not prevent that, so ignore those.

lisp/magit-git.el
@@ -1118,6 +1118,9 @@ tracked file."
   (magit-git-items "ls-files" "-z" "--others" "--ignored"
                    "--exclude-standard" "--directory"))
 
+(defun magit-stashed-files (stash)
+  (apply #'magit-git-items "stash" "show" "-z" "--name-only" stash))
+
 (defun magit-skip-worktree-files ()
   (--keep (and (= (aref it 0) ?S)
                (substring it 2))

lisp/magit-stash.el
@@ -241,7 +241,8 @@ If nothing is staged, then try to reinstate the stashed index.
 Doing so is not possible if there are staged changes."
   (interactive (list (magit-read-stash "Apply stash")))
   (magit-run-git "stash" "apply" stash
-                 (and (not (magit-anything-staged-p))
+                 (and (not (apply #'magit-anything-staged-p nil
+                                  (magit-stashed-files stash)))
                       "--index")))
 
 ;;;###autoload
@@ -252,7 +253,8 @@ Doing so is not possible if there are staged changes.  Do not
 remove the stash, if it cannot be applied."
   (interactive (list (magit-read-stash "Pop stash")))
   (magit-run-git "stash" "apply" stash
-                 (and (not (magit-anything-staged-p))
+                 (and (not (apply #'magit-anything-staged-p nil
+                                  (magit-stashed-files stash)))
                       "--index")))
 
 ;;;###autoload

commit f102acea61173d0577ad6010da7f26a40989fcee

magit-stashed-files: Call magit-git-items directly
Fixes a type error introduced in
f6f4b96b641a5af75e90b5431a32369ab0f664a7.
Since stash is a string (not a list), apply is not needed to call magit-git-items.

lisp/magit-git.el
@@ -1119,7 +1119,7 @@ tracked file."
                    "--exclude-standard" "--directory"))
 
 (defun magit-stashed-files (stash)
-  (apply #'magit-git-items "stash" "show" "-z" "--name-only" stash))
+  (magit-git-items "stash" "show" "-z" "--name-only" stash))
 
 (defun magit-skip-worktree-files ()
   (--keep (and (= (aref it 0) ?S)