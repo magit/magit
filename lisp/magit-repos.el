62c7ae0b5408311b8b69e48562733a9a1debd79f
Jonas Bernoulli <jonas@bernoul.li>
2023-09-19 16:53:38 +0200

    Alignment cosmetics

--- lisp/magit-repos.el
+++ lisp/magit-repos.el
@@ -265,8 +265,8 @@ If it contains \"%s\" then the directory is substituted for that."
 
 (define-derived-mode magit-repolist-mode tabulated-list-mode "Repos"
   "Major mode for browsing a list of Git repositories."
-  (setq-local x-stretch-cursor  nil)
-  (setq tabulated-list-padding  0)
+  (setq-local x-stretch-cursor nil)
+  (setq tabulated-list-padding 0)
   (add-hook 'tabulated-list-revert-hook #'magit-repolist-refresh nil t)
   (setq imenu-prev-index-position-function
         #'magit-repolist--imenu-prev-index-position)
