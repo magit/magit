ac3ff103a9806815fa1b14813da750b0d423a340
Jonas Bernoulli <jonas@bernoul.li>
2023-09-19 16:53:34 +0200

    Use pcase-setq

--- lisp/magit-extras.el
+++ lisp/magit-extras.el
@@ -713,21 +713,17 @@ the minibuffer too."
                             "1"))))
               pnt-args eob-args)
           (when (listp pnt-format)
-            (setq pnt-args (cdr pnt-format))
-            (setq pnt-format (car pnt-format)))
+            (pcase-setq `(,pnt-format . ,pnt-args) pnt-format))
           (when (listp eob-format)
-            (setq eob-args (cdr eob-format))
-            (setq eob-format (car eob-format)))
+            (pcase-setq `(,eob-format . ,eob-args) eob-format))
           (when pnt-format
             (when idx-format
-              (setq pnt-format
-                    (string-replace "%N" idx pnt-format)))
+              (setq pnt-format (string-replace "%N" idx pnt-format)))
             (magit-rev-insert-format pnt-format rev pnt-args)
             (delete-char -1))
           (when eob-format
             (when idx-format
-              (setq eob-format
-                    (string-replace "%N" idx eob-format)))
+              (setq eob-format (string-replace "%N" idx eob-format)))
             (save-excursion
               (goto-char (point-max))
               (skip-syntax-backward ">-")
