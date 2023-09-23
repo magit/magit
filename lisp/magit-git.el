3329bf0f4fbcdc9dc02f0c9b79f9038d4223cb4c
Jonas Bernoulli <jonas@bernoul.li>
2023-09-23 17:26:40 +0200

    magit-list-refs: Exclude all symbolic references
    Previously we included "origin" when using "%(refname:short)",
    because Git abbreviates "refs/remotes/origin/HEAD" to "origin".
    
      $ git for-each-ref "--format=%(refname:short)" refs/remotes/origin
      origin
      origin/main
    
      $ git for-each-ref "--format=%(refname)" refs/remotes/origin
      refs/remotes/origin/HEAD
      refs/remotes/origin/main

--- lisp/magit-git.el
+++ lisp/magit-git.el
@@ -1928,31 +1928,30 @@ where COMMITS is the number of commits in TAG but not in REV."
             str))))))
 
 (defun magit-list-refs (&optional namespaces format sortby)
-  "Return list of references.
+  "Return list of references, excluding symbolic references.
 
 When NAMESPACES is non-nil, list refs from these namespaces
 rather than those from `magit-list-refs-namespaces'.
 
 FORMAT is passed to the `--format' flag of `git for-each-ref'
-and defaults to \"%(refname)\".  If the format is \"%(refname)\"
-or \"%(refname:short)\", then drop the symbolic-ref `HEAD'.
+and defaults to \"%(refname)\".
 
 SORTBY is a key or list of keys to pass to the `--sort' flag of
 `git for-each-ref'.  When nil, use `magit-list-refs-sortby'"
   (unless format
     (setq format "%(refname)"))
-  (let ((refs (magit-git-lines "for-each-ref"
-                               (concat "--format=" format)
-                               (--map (concat "--sort=" it)
-                                      (pcase (or sortby magit-list-refs-sortby)
-                                        ((and val (pred stringp)) (list val))
-                                        ((and val (pred listp)) val)))
-                               (or namespaces magit-list-refs-namespaces))))
-    (if (member format '("%(refname)" "%(refname:short)"))
-        (let ((case-fold-search nil))
-          (--remove (string-match-p "\\(\\`\\|/\\)HEAD\\'" it)
-                    refs))
-      refs)))
+  (seq-keep (lambda (line)
+              (pcase-let* ((`(,symrefp ,value)
+                            (split-string line ""))
+                           (symrefp (not (equal symrefp ""))))
+                (and (not symrefp) value)))
+            (magit-git-lines "for-each-ref"
+                             (concat "--format=%(symref)" format)
+                             (--map (concat "--sort=" it)
+                                    (pcase (or sortby magit-list-refs-sortby)
+                                      ((and val (pred stringp)) (list val))
+                                      ((and val (pred listp)) val)))
+                             (or namespaces magit-list-refs-namespaces))))
 
 (defun magit-list-branches ()
   (magit-list-refs (list "refs/heads" "refs/remotes")))
