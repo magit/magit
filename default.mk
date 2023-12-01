c0e000d4ecad6638faf7886679da8fec1111716d
Jonas Bernoulli <jonas@bernoul.li>
2023-12-01 19:52:12 +0100

    Fix "Silence byte-compiler"

--- default.mk
+++ default.mk
@@ -26,8 +26,7 @@ TAR      ?= tar
 SED      ?= sed
 
 EMACS      ?= emacs
-EMACS_ARGS ?= --eval \
-"(remhash (indirect-function 'derived-mode-p) advertised-signature-table))"
+EMACS_ARGS ?=
 BATCH       = $(EMACS) -Q --batch $(EMACS_ARGS) $(LOAD_PATH)
 
 LISP_EXTRA_TARGETS ?= check-declare
