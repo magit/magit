058ca81497c2400db45644bfe3920bcaac21f1f1
Jonas Bernoulli <jonas@bernoul.li>
2024-04-28 20:21:03 +0200

    Bump magit-section dependency

--- lisp/magit-pkg.el
+++ lisp/magit-pkg.el
@@ -4,7 +4,7 @@
     (compat        "29.1.4.4")
     (dash          "20240103")
     (git-commit    "20240123")
-    (magit-section "20240114")
+    (magit-section "20240428")
     (seq           "2.24")
     (transient     "20240201")
     (with-editor   "20240101"))
