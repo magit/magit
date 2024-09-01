a5a3b4adfe670b15adc911d9cad694da46991c5d
kohnish <kohnish@gmx.com>
2024-09-01 21:37:01 +0200

    Remove magit-section from Package-Requires
    It's already provided and it is causing unnecessary installation from ELPA

--- lisp/magit.el
+++ lisp/magit.el
@@ -22,7 +22,6 @@
 ;;     (emacs "26.1")
 ;;     (compat "30.0.0.0")
 ;;     (dash "2.19.1")
-;;     (magit-section "4.1.0")
 ;;     (seq "2.24")
 ;;     (transient "0.7.5")
 ;;     (with-editor "3.4.2"))
