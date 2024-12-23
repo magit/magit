magit/magit

commit 621b67fc7e0663f262d17c8e975e7ebbe8fefbbe

magit-list-special-refnames: Cosmetics

lisp/magit-git.el
@@ -2011,10 +2011,8 @@ SORTBY is a key or list of keys to pass to the `--sort' flag of
 
 (defun magit-list-special-refnames ()
   (let ((gitdir (magit-gitdir)))
-    (cl-mapcan (lambda (name)
-                 (and (file-exists-p (expand-file-name name gitdir))
-                      (list name)))
-               magit-special-refnames)))
+    (seq-keep (lambda (name) (file-exists-p (expand-file-name name gitdir)))
+              magit-special-refnames)))
 
 (defun magit-list-branch-names ()
   (magit-list-refnames (list "refs/heads" "refs/remotes")))

commit 0e20be0a7255992df7cd932917bea48d9089a1cf

magit-list-special-refnames: Fix usage of seq-keep
seq-keep applies the function to the returned sequence, resulting a list of
'(t t t ...) being returned.  Adjust the lambda to return the ref when the ref
exists.

Before this fix, when using Push -> Another branch you would get "t" suggested
instead of "HEAD".

Fixes: 621b67fc (magit-list-special-refnames: Cosmetics, 2024-11-22)

lisp/magit-git.el
@@ -1981,7 +1981,9 @@ SORTBY is a key or list of keys to pass to the `--sort' flag of
 
 (defun magit-list-special-refnames ()
   (let ((gitdir (magit-gitdir)))
-    (seq-keep (lambda (name) (file-exists-p (expand-file-name name gitdir)))
+    (seq-keep (lambda (name)
+                (and (file-exists-p (expand-file-name name gitdir))
+                     name))
               magit-special-refnames)))
 
 (defun magit-list-branch-names ()