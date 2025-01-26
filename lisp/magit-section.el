a5962c0b916c1caf211a4f87fb85023068ab50b9
Jonas Bernoulli <jonas@bernoul.li>
2025-01-26 21:59:43 +0100

    magit-insert-section: Fix indent declaration

--- lisp/magit-section.el
+++ lisp/magit-section.el
@@ -1381,7 +1381,7 @@ a section by washing Git's output and Git didn't actually output
 anything this time around.
 
 \(fn [NAME] (CLASS &optional VALUE HIDE) &rest BODY)"
-  (declare (indent 1)
+  (declare (indent defun)
            (debug ([&optional symbolp]
                    (&or [("eval" form) &optional form form &rest form]
                         [symbolp &optional form form &rest form])
