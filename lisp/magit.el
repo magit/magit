2a789033ecc3d865ab1a4732d4e483caa1d4626c
Jonas Bernoulli <jonas@bernoul.li>
2025-04-01 19:52:19 +0200

    Bump dependencies

--- lisp/magit.el
+++ lisp/magit.el
@@ -21,10 +21,10 @@
 ;; Package-Requires: (
 ;;     (emacs "27.1")
 ;;     (compat "30.0.2.0")
-;;     (llama "0.6.1")
+;;     (llama "0.6.2")
 ;;     (magit-section "4.3.1")
 ;;     (seq "2.24")
-;;     (transient "0.8.5")
+;;     (transient "0.8.7")
 ;;     (with-editor "3.4.3"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
