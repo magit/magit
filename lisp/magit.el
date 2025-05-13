f88225f6a8731a5f950e7c6e4c955eddbf820c87
Jonas Bernoulli <jonas@bernoul.li>
2025-05-13 16:44:03 +0200

    Release version 4.3.4

--- lisp/magit.el
+++ lisp/magit.el
@@ -17,12 +17,12 @@
 ;; Homepage: https://github.com/magit/magit
 ;; Keywords: git tools vc
 
-;; Package-Version: 4.3.3
+;; Package-Version: 4.3.4
 ;; Package-Requires: (
 ;;     (emacs "27.1")
 ;;     (compat "30.1.0.0")
 ;;     (llama "0.6.2")
-;;     (magit-section "4.3.3")
+;;     (magit-section "4.3.4")
 ;;     (seq "2.24")
 ;;     (transient "0.8.8")
 ;;     (with-editor "3.4.3"))
