d9d2f6c312019c6056c6b771c190bf72ab6feaa7
Jonas Bernoulli <jonas@bernoul.li>
2025-07-01 15:34:59 +0200

    magit-after-save-refresh-status: Consult correct inhibit variable

--- lisp/magit-mode.el
+++ lisp/magit-mode.el
@@ -1213,7 +1213,7 @@ Note that refreshing a Magit buffer is done by re-creating its
 contents from scratch, which can be slow in large repositories.
 If you are not satisfied with Magit's performance, then you
 should obviously not add this function to that hook."
-  (when-let (((and (not magit--disable-save-buffers)
+  (when-let (((and (not magit-inhibit-refresh)
                    (magit-inside-worktree-p t)))
              (buf (ignore-errors (magit-get-mode-buffer 'magit-status-mode))))
     (cl-pushnew buf magit-after-save-refresh-buffers)
