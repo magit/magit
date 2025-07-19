56e2340923c7eed029a5840b07260a4f0a7e7e94
Jonas Bernoulli <jonas@bernoul.li>
2025-07-19 11:50:32 +0200

    magit-diff--goto-file-position: Use undefined return value
    Since the previous commit the return value doesn't matter anymore.
    Prior to that we should have used `and-let*' instead of `when-let'.

--- lisp/magit-diff.el
+++ lisp/magit-diff.el
@@ -1459,8 +1459,7 @@ for a revision."
             (t
              (goto-char (oref section start))
              (setq section (oref section parent))))
-      (magit-section-reveal section))
-    t))
+      (magit-section-reveal section))))
 
 ;;;; Setting Commands
 
