832d93b6bb647554384ae4ff01c91ff176aa030b
Jonas Bernoulli <jonas@bernoul.li>
2025-10-18 19:21:21 +0200

    Add support for triggering lisp hooks from git hooks

--- lisp/magit-process.el
+++ lisp/magit-process.el
@@ -529,12 +529,24 @@ eol conversion."
         (default-process-coding-system (magit--process-coding-system)))
     (apply #'process-file process infile buffer display args)))
 
+(defvar magit--shadowed-githook-directory nil)
+(defun magit--shadowed-githook-directory ()
+  (or magit--shadowed-githook-directory
+      (setq magit--shadowed-githook-directory
+            (let ((magit-git-global-arguments nil))
+              (cl-letf (((symbol-function 'magit-process-environment)
+                         (lambda () process-environment)))
+                (or (magit-get "core.hooksPath")
+                    (expand-file-name "hooks" (magit-gitdir))))))))
+
 (defun magit-process-environment ()
   ;; The various w32 hacks are only applicable when running on the local
   ;; machine.  A local binding of process-environment different from the
   ;; top-level value affects the environment used by Tramp.
   (let ((local (not (file-remote-p default-directory))))
     (append magit-git-environment
+            (list (concat "ORIG_HOOKS_PATH="
+                          (magit--shadowed-githook-directory)))
             (and local
                  (cdr (assoc magit-git-executable magit-git-w32-path-hack)))
             (and local magit-need-cygwin-noglob
