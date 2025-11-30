96f5c2b55503e6a4fb2d0ceebadbfc9cfc0aa79e
Jonas Bernoulli <jonas@bernoul.li>
2025-11-30 19:43:14 +0100

    make: No longer undeprecate if-let and when-let
    We now use the implementations from the Cond-Let package.

--- default.mk
+++ default.mk
@@ -28,9 +28,7 @@ TAR      ?= tar
 SED      ?= sed
 
 EMACS      ?= emacs
-EMACS_ARGS ?= --eval "(progn \
-  (put 'if-let 'byte-obsolete-info nil) \
-  (put 'when-let 'byte-obsolete-info nil))"
+EMACS_ARGS ?=
 BATCH       = $(EMACS) -Q --batch $(EMACS_ARGS) $(LOAD_PATH)
 
 LISP_EXTRA_TARGETS ?= check-declare
