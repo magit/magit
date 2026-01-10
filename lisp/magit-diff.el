3bddd5e5868fcb7be3c34f5d6324d25f86a84049
Jonas Bernoulli <jonas@bernoul.li>
2026-01-10 09:44:04 +0100

    magit-diff-refresh-buffer: Cosmetics

--- lisp/magit-diff.el
+++ lisp/magit-diff.el
@@ -2115,16 +2115,17 @@ Staging and applying changes is documented in info node
    (if (equal magit-buffer-typearg "--no-index")
        (apply #'format "Differences between %s and %s" magit-buffer-diff-files)
      (concat (cond (magit-buffer-range
-                    (if (string-match-p "\\(\\.\\.\\|\\^-\\)"
-                                        magit-buffer-range)
-                        (format "Changes in %s" magit-buffer-range)
-                      (let ((msg "Changes from %s to %s")
-                            (end (if (equal magit-buffer-typearg "--cached")
-                                     "index"
-                                   "working tree")))
-                        (if (member "-R" magit-buffer-diff-args)
-                            (format msg end magit-buffer-range)
-                          (format msg magit-buffer-range end)))))
+                    (cond-let
+                      ((string-match-p "\\(\\.\\.\\|\\^-\\)"
+                                       magit-buffer-range)
+                       (format "Changes in %s" magit-buffer-range))
+                      [[msg "Changes from %s to %s"]
+                       [end (if (equal magit-buffer-typearg "--cached")
+                                "index"
+                              "working tree")]]
+                      ((member "-R" magit-buffer-diff-args)
+                       (format msg end magit-buffer-range))
+                      ((format msg magit-buffer-range end))))
                    ((equal magit-buffer-typearg "--cached")
                     "Staged changes")
                    ((and (magit-repository-local-get 'this-commit-command)
