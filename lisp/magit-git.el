magit/magit

commit d527f8338613dd9ff7a43328e25065d1f3d7436e

magit-read-module-path: Add Helm kludge
Predicates are evaluate with the minibuffer as the current buffer.
Unlike for other completion frameworks, Helm does not ensure that
the value of `default-directory' in that buffer is the same as in
the buffer from which completion was invoked, making it necessary
to explicitly reinforce the expected value.

Closes #5546.

lisp/magit-git.el
@@ -2987,8 +2987,16 @@ out.  Only existing branches can be selected."
                              (magit-get-remote))))
 
 (defun magit-read-module-path (prompt &optional predicate)
+  ;; Predicates are evaluate with the minibuffer as the current buffer.
+  ;; Unlike for other completion frameworks, Helm does not ensure that
+  ;; the value of `default-directory' in that buffer is the same as in
+  ;; the buffer from which completion was invoked.
   (magit-completing-read prompt (magit-list-module-paths)
-                         predicate t nil nil
+                         (let ((dir default-directory))
+                           (lambda (module)
+                             (let ((default-directory dir))
+                               (funcall predicate module))))
+                         t nil nil
                          (magit-module-at-point predicate)))
 
 (defun magit-module-confirm (verb &optional predicate)

commit f0ead86bb4992b4bb56c48ba26b8b349d269053d

magit-read-module-path: Fix previous change
Only augment the predicate if there actually is one.

Fix problem introduced in d527f833.

lisp/magit-git.el
@@ -2994,10 +2994,11 @@ out.  Only existing branches can be selected."
   ;; the value of `default-directory' in that buffer is the same as in
   ;; the buffer from which completion was invoked.
   (magit-completing-read prompt (magit-list-module-paths)
-                         (let ((dir default-directory))
-                           (lambda (module)
-                             (let ((default-directory dir))
-                               (funcall predicate module))))
+                         (and predicate
+                              (let ((dir default-directory))
+                                (lambda (module)
+                                  (let ((default-directory dir))
+                                    (funcall predicate module)))))
                          t nil nil
                          (magit-module-at-point predicate)))
 